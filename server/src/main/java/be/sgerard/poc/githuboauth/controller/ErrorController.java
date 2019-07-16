b40bcb3f2b6d61f89f93487e317f12a020f92899
Sebastien Gerard <sgerard@emasphere.com>
2019-07-16 06:58:36 +0200

    ready

--- server/src/main/java/be/sgerard/poc/githuboauth/controller/ErrorController.java
+++ server/src/main/java/be/sgerard/poc/githuboauth/controller/ErrorController.java
@@ -2,11 +2,13 @@ package be.sgerard.poc.githuboauth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * @author Sebastien Gerard
  */
 @Controller
+@ApiIgnore
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping(value = "/error")
