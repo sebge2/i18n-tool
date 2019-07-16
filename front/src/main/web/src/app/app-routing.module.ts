ec36ba11c1d8391d4f2c0304e22c898e7657e8c2
Sebastien Gerard <sgerard@emasphere.com>
2019-07-16 13:05:55 +0200

    front

--- front/src/main/web/src/app/app-routing.module.ts
+++ front/src/main/web/src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {GlobalAuthGuard} from "./core/ui/service/guard/global-auth-guard.service";
 
 const routes: Routes = [
-    {path: '', pathMatch: 'full', redirectTo: '/translations', canActivate: [GlobalAuthGuard]},
+    {path: '', pathMatch: 'full', redirectTo: '/translations'},
     {path: '', loadChildren: './core/ui/core-ui.module#CoreUiModule'},
     {path: 'error', loadChildren: './error/core-error.module#CoreErrorModule'},
     {path: '**', redirectTo: "/error/404"}
--- front/src/main/web/src/app/core/ui/core-ui.module.ts
+++ front/src/main/web/src/app/core/ui/core-ui.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainComponent} from './component/main/main.component';
 import {RouterModule, Routes} from "@angular/router";
+import {GlobalAuthGuard} from "./service/guard/global-auth-guard.service";
 
 const appRoutes: Routes = [
     {
         path: '',
         component: MainComponent,
+        canActivate: [GlobalAuthGuard],
         children: [
             {path: 'translations', loadChildren: './../../translations/translations.module#TranslationsModule'},
             {path: 'settings', loadChildren: './../../settings/settings.module#SettingsModule'}
