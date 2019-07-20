a420febb890928a03d85a61d68ac00f757b74111
Sebastien Gerard <sgerard@emasphere.com>
2019-07-20 16:53:17 +0200

    model

--- front/src/main/web/src/app/core/auth/model/user-session.model.ts
+++ front/src/main/web/src/app/core/auth/model/user-session.model.ts
@@ -2,37 +2,18 @@ import {User} from "./user.model";
 
 export class UserSession {
 
-  private id: string;
-  private user: User;
-  private simpSessionId: string;
-  private loginTime: Date;
-  private logoutTime: Date;
+    readonly id: string;
+    readonly user: User;
+    readonly simpSessionId: string;
+    readonly loginTime: Date;
+    readonly logoutTime: Date;
 
-  constructor(userSession: UserSession = <UserSession>{}) {
-    this.id = userSession.id;
-    this.user = new User(userSession.user);
-    this.simpSessionId = userSession.simpSessionId;
-    this.loginTime = userSession.loginTime;
-    this.logoutTime= userSession.logoutTime;
-  }
+    constructor(userSession: UserSession = <UserSession>{}) {
+        this.id = userSession.id;
+        this.user = new User(userSession.user);
+        this.simpSessionId = userSession.simpSessionId;
+        this.loginTime = userSession.loginTime;
+        this.logoutTime = userSession.logoutTime;
+    }
 
-  getId(): string {
-    return this.id;
-  }
-
-  getUser(): User {
-    return this.user;
-  }
-
-  getSimpSessionId(): string {
-    return this.simpSessionId;
-  }
-
-  getLoginTime(): Date {
-    return this.loginTime;
-  }
-
-  getLogoutTime(): Date {
-    return this.logoutTime;
-  }
 }
--- front/src/main/web/src/app/core/auth/model/user.model.ts
+++ front/src/main/web/src/app/core/auth/model/user.model.ts
@@ -1,21 +1,21 @@
 export class User {
 
-  id: string;
-  username: string;
-  email: string;
-  avatarUrl: string;
-  roles: string[];
+    readonly id: string;
+    readonly username: string;
+    readonly email: string;
+    readonly avatarUrl: string;
+    readonly roles: string[];
 
-  constructor(user: User = <User>{}) {
-    this.id = user.id;
-    this.username = user.username;
-    this.email = user.email;
-    this.avatarUrl = user.avatarUrl;
+    constructor(user: User = <User>{}) {
+        this.id = user.id;
+        this.username = user.username;
+        this.email = user.email;
+        this.avatarUrl = user.avatarUrl;
 
-    this.roles =
-      (user.roles != null)
-        ? user.roles
-        : [];
-  }
+        this.roles =
+            (user.roles != null)
+                ? user.roles
+                : [];
+    }
 
 }
