2800b2ee3b2ab9f690eec6b1b7150dc6c7409afa
Sebastien Gerard <sgerard@emasphere.com>
2019-07-21 20:27:08 +0200

    model

---
+++ front/src/main/web/src/app/translations/service/workspace.service.ts
@@ -0,0 +1,28 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {EventService} from "../../core/event/service/event.service";
+import {Workspace} from "../model/workspace.model";
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WorkspaceService implements OnDestroy {
+
+    private _workspaces: BehaviorSubject<Workspace[]> = new BehaviorSubject<Workspace[]>([]);
+
+    constructor(private httpClient: HttpClient,
+                private eventService: EventService) {
+        this.httpClient.get<Workspace[]>('/api/workspace').toPromise()
+            .then(workspaces => this._workspaces.next(workspaces))
+            .catch(reason => console.error("Error while retrieving workspaces.", reason));
+
+    }
+
+    ngOnDestroy(): void {
+    }
+
+    getWorkspaces(): BehaviorSubject<Workspace[]> {
+        return this._workspaces;
+    }
+}
