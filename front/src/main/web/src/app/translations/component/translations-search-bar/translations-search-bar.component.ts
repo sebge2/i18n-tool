dabc8f3d6633efbf5c973d424af8041c725293a1
Sebastien Gerard <sgerard@emasphere.com>
2019-07-30 20:20:37 +0200

    search bar

--- front/src/main/web/src/app/translations/component/translations-search-bar/translations-search-bar.component.ts
+++ front/src/main/web/src/app/translations/component/translations-search-bar/translations-search-bar.component.ts
@@ -1,13 +1,15 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {Locale} from "../../model/locale.model";
+import {ALL_LOCALES, Locale} from "../../model/locale.model";
 import {Workspace} from "../../model/workspace.model";
 import {TranslationsSearchRequest} from "../../model/translations-search-request.model";
 import {TranslationsSearchCriterion} from "../../model/translations-search-criterion.model";
+import {LocaleIconPipe} from "../../pipe/locale-icon.pipe";
 
 @Component({
     selector: 'app-translations-search-bar',
     templateUrl: './translations-search-bar.component.html',
-    styleUrls: ['./translations-search-bar.component.css']
+    styleUrls: ['./translations-search-bar.component.css'],
+    providers: [LocaleIconPipe]
 })
 export class TranslationsSearchBarComponent implements OnInit {
 
@@ -16,7 +18,7 @@ export class TranslationsSearchBarComponent implements OnInit {
 
     searchRequest: TranslationsSearchRequest = new TranslationsSearchRequest();
 
-    constructor() {
+    constructor(private localeIconPipe: LocaleIconPipe) {
         this.searchRequest.criterion = TranslationsSearchCriterion.MISSING_TRANSLATIONS;
         this.searchRequest.locales = [Locale.FR, Locale.EN];
     }
@@ -34,6 +36,47 @@ export class TranslationsSearchBarComponent implements OnInit {
 
     onSelectedCriterion(criterion: TranslationsSearchCriterion) {
         this.searchRequest.criterion = criterion;
+    }
+
+    title(): String {
+        let title = "Search";
+
+        switch (this.searchRequest.criterion) {
+            case TranslationsSearchCriterion.ALL:
+                title += " for <b>all</b> translations";
+                break;
+            case TranslationsSearchCriterion.MISSING_TRANSLATIONS:
+                title += " for <b>missing</b> translations";
+                break;
+            case TranslationsSearchCriterion.TRANSLATIONS_CURRENT_USER_UPDATED:
+                title += " for translations that <b>I have updated</b>";
+                break;
+            case TranslationsSearchCriterion.UPDATED_TRANSLATIONS:
+                title += " for <b>updated</b> translations";
+                break;
+        }
+
+        if (this.searchRequest.workspace != null) {
+            title += " on " + this.searchRequest.workspace.branch + " branch";
+        }
+
+        if (this.searchRequest.locales.length == 0 || this.searchRequest.locales.length == ALL_LOCALES.length) {
+            title += " for <b>all locales</b>";
+        } else {
+            title += " for locales ";
+
+            for (let i = 0; i < this.searchRequest.locales.length; i++) {
+                if (i == this.searchRequest.locales.length - 1 && i > 0) {
+                    title += " and ";
+                } else if (i > 0) {
+                    title += ",";
+                }
+
+                title += " <span class=\"" + this.localeIconPipe.transform(this.searchRequest.locales[i]) + "\"></span>" + this.searchRequest.locales[i];
+            }
+        }
+
+        return title + ".";
     }
 
     onSearch() {
