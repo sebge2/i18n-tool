54f861db80390dace50491a769cd336c6ff6e1ac
Sebastien Gerard <sgerard@emasphere.com>
2019-07-31 19:55:49 +0200

    model

--- front/src/main/web/src/app/translations/component/translations-table/translations-table.component.ts
+++ front/src/main/web/src/app/translations/component/translations-table/translations-table.component.ts
@@ -1,20 +1,33 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {TranslationsSearchRequest} from "../../model/translations-search-request.model";
+import {TranslationsService} from '../../service/translations.service';
+import {BundleKeysPage} from "../../model/edition/bundle-keys-page.model";
 
 @Component({
     selector: 'app-translations-table',
     templateUrl: './translations-table.component.html',
     styleUrls: ['./translations-table.component.css']
 })
-export class TranslationsTableComponent implements OnInit {
+export class TranslationsTableComponent implements OnInit, OnChanges {
 
     @Input()
     searchRequest: TranslationsSearchRequest = new TranslationsSearchRequest();
 
-    constructor() {
+    bundleKeysPage: BundleKeysPage;
+
+    constructor(private translationsService: TranslationsService) {
     }
 
     ngOnInit() {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.searchRequest != null && this.searchRequest.workspace != null) {
+            this.translationsService
+                .getTranslations(this.searchRequest)
+                .toPromise()
+                .then(page => this.bundleKeysPage = page);
+        }
     }
 
 }
---
+++ front/src/main/web/src/app/translations/service/translations.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {TranslationsSearchRequest} from "../model/translations-search-request.model";
+import {BundleKeysPage} from "../model/edition/bundle-keys-page.model";
+import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TranslationsService {
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    getTranslations(searchRequest: TranslationsSearchRequest,
+                    lastPage?: BundleKeysPage,
+                    maxKeys?: number): Observable<BundleKeysPage> {
+        let params = new HttpParams();
+
+        if (searchRequest.criterion != null) {
+            params = params.set("criterion", searchRequest.criterion);
+        }
+
+        if (lastPage != null) {
+            params = params.set("lastKey", String(lastPage.lastKey));
+        }
+
+        if (maxKeys != null) {
+            params = params.set("maxKeys", String(maxKeys));
+        }
+
+        for (const locale of searchRequest.locales) {
+            params = params.append("locales", locale);
+        }
+
+        return this.httpClient
+            .get<BundleKeysPage>(
+                '/api/workspace/' + searchRequest.workspace.id + '/translation',
+                {params: params}
+            )
+            .pipe(map(page => new BundleKeysPage(page)));
+    }
+
+}
