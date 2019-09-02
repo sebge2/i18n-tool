f191c3e27e3c097768a9480f502adb2e793fe6f4
Sebastien Gerard <sgerard@emasphere.com>
2019-09-02 18:49:03 +0200

    repo

---
+++ front/src/main/web/src/app/admin/component/admin/admin.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
