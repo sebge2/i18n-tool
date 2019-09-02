20f26c0f9b30dd4c7229398da2bb9213e47c9073
Sebastien Gerard <sgerard@emasphere.com>
2019-09-02 21:06:57 +0200

    repo

--- front/src/main/web/src/app/core/event/model.events.model.ts
+++ front/src/main/web/src/app/core/event/model.events.model.ts
@@ -2,5 +2,7 @@ export enum Events {
 
     UPDATED_WORKSPACE = "updated-workspace",
 
-    DELETED_WORKSPACE = "deleted-workspace"
+    DELETED_WORKSPACE = "deleted-workspace",
+
+    UPDATED_REPOSITORY = "updated-repository"
 }
--- front/src/main/web/src/app/translations/service/repository.service.ts
+++ front/src/main/web/src/app/translations/service/repository.service.ts
@@ -1,19 +1,35 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {EventService} from "../../core/event/service/event.service";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Subscription} from "rxjs";
 import {Repository} from "../model/repository.model";
+import {Events} from "../../core/event/model.events.model";
 
 @Injectable({
     providedIn: 'root'
 })
-export class RepositoryService {
+export class RepositoryService implements OnDestroy {
 
-    private _repository: BehaviorSubject<Repository> = new BehaviorSubject<Repository>(new Repository(<Repository> {initialized: false}));
+    private _repositorySubscription: Subscription;
+
+    private _repository: BehaviorSubject<Repository> = new BehaviorSubject<Repository>(new Repository(<Repository>{initialized: false}));
 
     constructor(private httpClient: HttpClient,
                 private eventService: EventService) {
-        // TODO
+        this.httpClient.get<Repository>('/api/repository').toPromise()
+            .then(repository => this._repository.next(new Repository(repository)))
+            .catch(reason => console.error("Error while retrieving the repository.", reason));
+
+        this._repositorySubscription = this.eventService.subscribe(Events.UPDATED_REPOSITORY, Repository)
+            .subscribe(
+                (repository: Repository) => {
+                    this._repository.next(repository);
+                }
+            );
+    }
+
+    ngOnDestroy(): void {
+        this._repositorySubscription.unsubscribe();
     }
 
     getRepository(): BehaviorSubject<Repository> {
