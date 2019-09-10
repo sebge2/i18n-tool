32e56f334066a59b3da0021547c26334e63b39bf
Sebastien Gerard <sgerard@emasphere.com>
2019-09-10 19:31:22 +0200

    tests

--- front/src/main/web/src/app/admin/component/admin/admin.component.spec.ts
+++ front/src/main/web/src/app/admin/component/admin/admin.component.spec.ts
@@ -1,25 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AdminComponent } from './admin.component';
+import {AdminComponent} from './admin.component';
+import {RepositoryInitializerComponent} from "../repository-initializer/repository-initializer.component";
+import {WorkspaceTableComponent} from "../workspace-table/workspace-table.component";
+import {TranslateModule} from "@ngx-translate/core";
+import {CoreSharedModule} from "../../../core/shared/core-shared-module";
+import {HttpClientModule} from "@angular/common/http";
+import {CoreEventModule} from "../../../core/event/core-event.module";
+import {CoreUiModule} from "../../../core/ui/core-ui.module";
 
 describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
+    let component: AdminComponent;
+    let fixture: ComponentFixture<AdminComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                CoreUiModule,
+                CoreSharedModule,
+                CoreEventModule,
+                HttpClientModule,
+                TranslateModule.forRoot()
+            ],
+            declarations: [
+                AdminComponent,
+                RepositoryInitializerComponent,
+                WorkspaceTableComponent
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AdminComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
