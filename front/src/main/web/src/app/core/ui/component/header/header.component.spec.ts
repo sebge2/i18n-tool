a49c3bf27e229669716c734d0e3432b47662757f
Sebastien Gerard <sgerard@emasphere.com>
2019-09-10 19:28:38 +0200

    tests

--- front/src/main/web/src/app/core/ui/component/header/header.component.spec.ts
+++ front/src/main/web/src/app/core/ui/component/header/header.component.spec.ts
@@ -1,25 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CoreEventModule} from "../../../event/core-event.module";
+import {CoreUiModule} from "../../core-ui.module";
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                CoreUiModule,
+                CoreEventModule,
+                HttpClientModule
+            ],
+            declarations: []
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- front/src/main/web/src/app/core/ui/component/main/main.component.spec.ts
+++ front/src/main/web/src/app/core/ui/component/main/main.component.spec.ts
@@ -1,25 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import {MainComponent} from './main.component';
+import {CoreSharedModule} from "../../../shared/core-shared-module";
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {CoreEventModule} from "../../../event/core-event.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CoreUiModule} from "../../core-ui.module";
 
 describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+    let component: MainComponent;
+    let fixture: ComponentFixture<MainComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserAnimationsModule,
+                CoreSharedModule,
+                CoreUiModule,
+                CoreEventModule,
+                RouterModule.forRoot([]),
+                HttpClientModule
+            ],
+            declarations: [],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MainComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
