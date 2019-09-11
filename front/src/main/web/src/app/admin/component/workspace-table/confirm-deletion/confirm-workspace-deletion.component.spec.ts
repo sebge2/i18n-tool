ede2ddfc2f8b833ea35519061634e1433b7b714d
Sebastien Gerard <sgerard@emasphere.com>
2019-09-11 07:03:04 +0200

    tests

--- front/src/main/web/src/app/admin/component/workspace-table/confirm-deletion/confirm-workspace-deletion.component.spec.ts
+++ front/src/main/web/src/app/admin/component/workspace-table/confirm-deletion/confirm-workspace-deletion.component.spec.ts
@@ -1,25 +1,74 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
-import { ConfirmWorkspaceDeletionComponent } from './confirm-workspace-deletion.component';
+import {ConfirmWorkspaceDeletionComponent} from './confirm-workspace-deletion.component';
+import {CoreSharedModule} from "../../../../core/shared/core-shared-module";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
+import {Workspace} from "../../../../translations/model/workspace.model";
+import {OverlayContainer} from "@angular/cdk/overlay";
+import {CoreUiModule} from "../../../../core/ui/core-ui.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {TranslateModule} from "@ngx-translate/core";
 
 describe('ConfirmDeletionComponent', () => {
-  let component: ConfirmWorkspaceDeletionComponent;
-  let fixture: ComponentFixture<ConfirmWorkspaceDeletionComponent>;
+    let dialog: MatDialog;
+    let overlayContainer: OverlayContainer;
+    let component: ConfirmWorkspaceDeletionComponent;
+    let fixture: ComponentFixture<ConfirmWorkspaceDeletionComponent>;
+    const mockDialogRef = {
+        close: jasmine.createSpy('close')
+    };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ConfirmWorkspaceDeletionComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserAnimationsModule,
+                CoreUiModule,
+                CoreSharedModule,
+                TranslateModule.forRoot()
+            ],
+            providers: [
+                {provide: MatDialogRef, useValue: mockDialogRef},
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {workspace: new Workspace(<Workspace>{branch: 'master'})}
+                }
+            ],
+            declarations: [ConfirmWorkspaceDeletionComponent],
+        });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmWorkspaceDeletionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.overrideModule(BrowserDynamicTestingModule, {
+            set: {
+                entryComponents: [ConfirmWorkspaceDeletionComponent]
+            }
+        });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(inject([MatDialog, OverlayContainer],
+        (d: MatDialog, oc: OverlayContainer) => {
+            dialog = d;
+            overlayContainer = oc;
+        })
+    );
+
+    afterEach(() => {
+        overlayContainer.ngOnDestroy();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ConfirmWorkspaceDeletionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('onCancel should close the dialog', () => {
+        component.onCancel();
+        expect(mockDialogRef.close).toHaveBeenCalled();
+    });
 });
--- front/src/main/web/src/app/translations/component/translations-start-review/translations-start-review.component.spec.ts
+++ front/src/main/web/src/app/translations/component/translations-start-review/translations-start-review.component.spec.ts
@@ -1,25 +1,73 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
-import { TranslationsStartReviewComponent } from './translations-start-review.component';
+import {TranslationsStartReviewComponent} from './translations-start-review.component';
+import {TranslateModule} from "@ngx-translate/core";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material";
+import {OverlayContainer} from "@angular/cdk/overlay";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CoreUiModule} from "../../../core/ui/core-ui.module";
+import {CoreSharedModule} from "../../../core/shared/core-shared-module";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
 
 describe('TranslationsStartReviewComponent', () => {
-  let component: TranslationsStartReviewComponent;
-  let fixture: ComponentFixture<TranslationsStartReviewComponent>;
+    let dialog: MatDialog;
+    let overlayContainer: OverlayContainer;
+    let component: TranslationsStartReviewComponent;
+    let fixture: ComponentFixture<TranslationsStartReviewComponent>;
+    const mockDialogRef = {
+        close: jasmine.createSpy('close')
+    };
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TranslationsStartReviewComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserAnimationsModule,
+                CoreUiModule,
+                CoreSharedModule,
+                TranslateModule.forRoot()
+            ],
+            providers: [
+                {provide: MatDialogRef, useValue: mockDialogRef},
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {comment: null}
+                }
+            ],
+            declarations: [TranslationsStartReviewComponent],
+        });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TranslationsStartReviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        TestBed.overrideModule(BrowserDynamicTestingModule, {
+            set: {
+                entryComponents: [TranslationsStartReviewComponent]
+            }
+        });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.compileComponents();
+    }));
+
+    beforeEach(inject([MatDialog, OverlayContainer],
+        (d: MatDialog, oc: OverlayContainer) => {
+            dialog = d;
+            overlayContainer = oc;
+        })
+    );
+
+    afterEach(() => {
+        overlayContainer.ngOnDestroy();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TranslationsStartReviewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('onCancel should close the dialog', () => {
+        component.onCancel();
+        expect(mockDialogRef.close).toHaveBeenCalled();
+    });
 });
