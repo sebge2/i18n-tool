dd89bb720fa33afd9ad188022f63f5197bd9e164
Sebastien Gerard <sgerard@emasphere.com>
2019-09-17 21:13:21 +0200

    tests

--- front/src/main/web/src/app/core/auth/service/authentication.service.spec.ts
+++ front/src/main/web/src/app/core/auth/service/authentication.service.spec.ts
@@ -1,13 +1,45 @@
-import {TestBed} from '@angular/core/testing';
+import {getTestBed, inject, TestBed} from '@angular/core/testing';
 
 import {AuthenticationService} from './authentication.service';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {User} from "../model/user.model";
 
 describe('AuthenticationService', () => {
-    beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientModule]}));
+    let injector: TestBed;
+    let service: AuthenticationService;
+    let httpMock: HttpTestingController;
 
-    it('should be created', () => {
-        const service: AuthenticationService = TestBed.get(AuthenticationService);
-        expect(service).toBeTruthy();
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [AuthenticationService]
+        });
+
+        injector = getTestBed();
+        service = injector.get(AuthenticationService);
+        httpMock = injector.get(HttpTestingController);
     });
+
+    it('should get current user',
+        inject(
+            [HttpTestingController, AuthenticationService],
+            async (
+                httpMock: HttpTestingController,
+                authService: AuthenticationService
+            ) => {
+                const expected = new User(<User>{id: 'abc'});
+
+                const promise = authService.currentUser
+                    .toPromise()
+                    .then((actual: User) => {
+                        expect(actual).toEqual(expected);
+                    });
+
+                httpMock.expectOne('/api/authentication/user').flush(expected);
+                httpMock.verify();
+
+                return promise;
+            }
+        )
+    );
 });
--- front/src/main/web/src/app/core/auth/service/authentication.service.ts
+++ front/src/main/web/src/app/core/auth/service/authentication.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user.model";
+import {map} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -12,6 +13,7 @@ export class AuthenticationService {
     }
 
     get currentUser(): Observable<User> {
-        return this.httpClient.get<User>('/api/authentication/user');
+        return this.httpClient.get<User>('/api/authentication/user')
+            .pipe(map(user => new User(user)));
     }
 }
