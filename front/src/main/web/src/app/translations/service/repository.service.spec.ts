6c3a3a21668441b85bf78227eaf1bc0a4d26466a
Sebastien Gerard <sgerard@emasphere.com>
2019-09-18 06:44:58 +0200

    tests

--- front/src/main/web/src/app/translations/service/repository.service.spec.ts
+++ front/src/main/web/src/app/translations/service/repository.service.spec.ts
@@ -1,4 +1,4 @@
-import {getTestBed, inject, TestBed} from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
 import {RepositoryService} from './repository.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
@@ -45,88 +45,79 @@ describe('RepositoryService', () => {
     });
 
     it('should get repository',
-        inject(
-            [HttpTestingController, RepositoryService],
-            async () => {
-                const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
-                const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
+        async () => {
+            const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
+            const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
 
-                const promise = service.getRepository()
-                    .pipe(take(2), toArray())
-                    .toPromise()
-                    .then((actual: Repository[]) => {
-                        expect(actual).toEqual([firstExpected, secondExpected]);
-                    });
+            const promise = service.getRepository()
+                .pipe(take(2), toArray())
+                .toPromise()
+                .then((actual: Repository[]) => {
+                    expect(actual).toEqual([firstExpected, secondExpected]);
+                });
 
-                httpMock.expectOne('/api/repository').flush(secondExpected);
-                httpMock.verify();
+            httpMock.expectOne('/api/repository').flush(secondExpected);
+            httpMock.verify();
 
-                return promise;
-            }
-        )
+            return promise;
+        }
     );
 
     it('should get latest repository',
-        inject(
-            [HttpTestingController, RepositoryService],
-            async () => {
-                const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
-                const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
+        async () => {
+            const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
+            const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
 
-                const firstPromise = service.getRepository()
-                    .pipe(take(2), toArray())
-                    .toPromise()
-                    .then(actual => {
-                        expect(actual).toEqual([firstExpected, secondExpected]);
-                        return actual;
-                    });
+            const firstPromise = service.getRepository()
+                .pipe(take(2), toArray())
+                .toPromise()
+                .then(actual => {
+                    expect(actual).toEqual([firstExpected, secondExpected]);
+                    return actual;
+                });
 
-                httpMock.expectOne('/api/repository').flush(secondExpected);
-                httpMock.verify();
+            httpMock.expectOne('/api/repository').flush(secondExpected);
+            httpMock.verify();
 
-                await firstPromise;
+            await firstPromise;
 
-                const secondPromise = service.getRepository()
-                    .pipe(take(1), toArray())
-                    .toPromise()
-                    .then(actual => {
-                        expect(actual).toEqual([secondExpected]);
-                        return actual;
-                    });
+            const secondPromise = service.getRepository()
+                .pipe(take(1), toArray())
+                .toPromise()
+                .then(actual => {
+                    expect(actual).toEqual([secondExpected]);
+                    return actual;
+                });
 
-                await secondPromise;
-            }
-        )
+            await secondPromise;
+        }
     );
 
     it('should get latest updated repository',
-        inject(
-            [HttpTestingController, RepositoryService],
-            async () => {
-                const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
-                const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZING});
-                const thirdExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
+        async () => {
+            const firstExpected = new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED});
+            const secondExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZING});
+            const thirdExpected = new Repository(<Repository>{status: RepositoryStatus.INITIALIZED});
 
-                const promise = service.getRepository()
-                    .pipe(take(3), toArray())
-                    .toPromise()
-                    .then(actual => {
-                        expect(actual).toEqual([firstExpected, secondExpected, thirdExpected]);
-                        return actual;
-                    });
+            const promise = service.getRepository()
+                .pipe(take(3), toArray())
+                .toPromise()
+                .then(actual => {
+                    expect(actual).toEqual([firstExpected, secondExpected, thirdExpected]);
+                    return actual;
+                });
 
-                httpMock.expectOne('/api/repository').flush(secondExpected);
-                httpMock.verify();
+            httpMock.expectOne('/api/repository').flush(secondExpected);
+            httpMock.verify();
 
-                service.getRepository()
-                    .pipe(take(2))
-                    .toPromise()
-                    .then(actual => {
-                        eventService.subject.next(thirdExpected);
-                    });
+            service.getRepository()
+                .pipe(take(2))
+                .toPromise()
+                .then(actual => {
+                    eventService.subject.next(thirdExpected);
+                });
 
-                return promise;
-            }
-        )
+            return promise;
+        }
     );
 });
