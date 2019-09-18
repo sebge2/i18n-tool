ca0ce18f774caf99539471f39b5db8408b8b2d2d
Sebastien Gerard <sgerard@emasphere.com>
2019-09-18 21:01:00 +0200

    tests

--- front/src/main/web/src/app/admin/component/workspace-table/workspace-table.component.spec.ts
+++ front/src/main/web/src/app/admin/component/workspace-table/workspace-table.component.spec.ts
@@ -6,32 +6,88 @@ import {CoreSharedModule} from "../../../core/shared/core-shared-module";
 import {HttpClientModule} from "@angular/common/http";
 import {CoreEventModule} from "../../../core/event/core-event.module";
 import {CoreUiModule} from "../../../core/ui/core-ui.module";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
+import {Workspace} from "../../../translations/model/workspace.model";
+import {WorkspaceService} from "../../../translations/service/workspace.service";
 
 describe('WorkspaceTableComponent', () => {
     let component: WorkspaceTableComponent;
     let fixture: ComponentFixture<WorkspaceTableComponent>;
+    let workspaceService: MockWorkspaceService;
+
+    class MockWorkspaceService {
+
+        readonly subject: Subject<Workspace[]> = new BehaviorSubject([]);
+
+        public getWorkspaces(): Observable<Workspace[]> {
+            return this.subject;
+        }
+
+        public find(): Promise<any> {
+            return Promise.resolve(null);
+        }
+    }
 
     beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                TranslateModule.forRoot(),
-                CoreUiModule,
-                CoreSharedModule,
-                CoreEventModule,
-                HttpClientModule
-            ],
-            declarations: [WorkspaceTableComponent]
-        })
-            .compileComponents();
-    }));
+        workspaceService = new MockWorkspaceService();
 
-    beforeEach(() => {
+        TestBed
+            .configureTestingModule({
+                imports: [
+                    TranslateModule.forRoot(),
+                    CoreUiModule,
+                    CoreSharedModule,
+                    CoreEventModule,
+                    HttpClientModule
+                ],
+                providers: [
+                    {provide: WorkspaceService, useValue: workspaceService}
+                ],
+                declarations: [WorkspaceTableComponent]
+            })
+            .compileComponents();
+
         fixture = TestBed.createComponent(WorkspaceTableComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    }));
 
-    it('should create', () => {
-        expect(component).toBeTruthy(); // TODO
-    });
+    it('should display workspaces',
+        async () => {
+            workspaceService.subject.next(
+                [
+                    new Workspace(<Workspace>{id: 'abc', branch: 'master'}),
+                    new Workspace(<Workspace>{id: 'def', branch: 'release/2019.9'})
+                ]
+            );
+
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                const rows = fixture.debugElement.nativeElement.querySelectorAll('mat-row');
+
+                expect(rows.length).toBe(2);
+                expect(rows[0].querySelector('mat-cell').textContent).toContain('master');
+                expect(rows[1].querySelector('mat-cell').textContent).toContain('release/2019.9');
+            });
+        });
+
+    it('should find when clicked',
+        async () => {
+            workspaceService.subject.next(
+                [
+                    new Workspace(<Workspace>{id: 'abc', branch: 'master'}),
+                    new Workspace(<Workspace>{id: 'def', branch: 'release/2019.9'})
+                ]
+            );
+
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+                spyOn(workspaceService, 'find').and.returnValue(Promise.resolve());
+
+                fixture.debugElement.nativeElement.querySelector('#findButton').click();
+
+                expect(workspaceService.find).toHaveBeenCalled();
+            });
+        });
+
+    // TODO erase workspace
 });
