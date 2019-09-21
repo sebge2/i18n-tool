5fb30d06a508a2e99d3f36a15d9774c858ebaa58
Sebastien Gerard <sgerard@emasphere.com>
2019-09-21 08:16:42 +0200

    tests

--- front/src/main/web/src/app/core/auth/service/user-session.service.spec.ts
+++ front/src/main/web/src/app/core/auth/service/user-session.service.spec.ts
@@ -17,13 +17,18 @@ describe('UserSessionService', () => {
 
     class MockEventService {
 
-        readonly subject: Subject<UserSession> = new Subject();
+        readonly connected: Subject<UserSession> = new Subject();
+        readonly disconnected: Subject<UserSession> = new Subject();
 
         public subscribe(eventType: string, type: any): Observable<UserSession> {
             expect(eventType).toMatch(new RegExp(Events.CONNECTED_USER_SESSION + '|' + Events.DISCONNECTED_USER_SESSION));
             expect(type).toEqual(UserSession);
 
-            return this.subject;
+            if(eventType == Events.CONNECTED_USER_SESSION){
+                return this.connected;
+            } else {
+                return this.disconnected;
+            }
         }
     }
 
@@ -65,6 +70,70 @@ describe('UserSessionService', () => {
         }
     );
 
-    // TODO event connected
-    // TODO event disconnected
+    it('should get user sessions after connection',
+        async () => {
+            const sessions: UserSession[] = [
+                new UserSession(<UserSession>{id: 'ghi'}),
+                new UserSession(<UserSession>{id: 'def'}),
+                new UserSession(<UserSession>{id: 'abc'})
+            ];
+
+            const firstPromise = service.getCurrentUserSessions()
+                .pipe(take(2), toArray())
+                .toPromise()
+                .then((actual: UserSession[][]) => {
+                    expect(actual).toEqual([[], sessions]);
+                });
+
+            httpMock.expectOne('/api/user-session/current').flush(sessions);
+            httpMock.verify();
+
+            await firstPromise;
+
+            const newConnection = new UserSession(<UserSession>{id: 'jkl'});
+
+            eventService.connected.next(newConnection);
+
+            return service.getCurrentUserSessions()
+                .pipe(take(1), toArray())
+                .toPromise()
+                .then((actual: UserSession[][]) => {
+                    expect(actual).toEqual([sessions.concat(newConnection)]);
+                });
+        }
+    );
+
+    it('should get user sessions after disconnection',
+        async () => {
+            const sessions: UserSession[] = [
+                new UserSession(<UserSession>{id: 'ghi'}),
+                new UserSession(<UserSession>{id: 'def'}),
+                new UserSession(<UserSession>{id: 'abc'})
+            ];
+
+            const firstPromise = service.getCurrentUserSessions()
+                .pipe(take(2), toArray())
+                .toPromise()
+                .then((actual: UserSession[][]) => {
+                    expect(actual).toEqual([[], sessions]);
+                });
+
+            httpMock.expectOne('/api/user-session/current').flush(sessions);
+            httpMock.verify();
+
+            await firstPromise;
+
+            eventService.disconnected.next(sessions[0]);
+
+            return service.getCurrentUserSessions()
+                .pipe(take(1), toArray())
+                .toPromise()
+                .then((actual: UserSession[][]) => {
+                    const expected = sessions.slice();
+                    expected.splice(0, 1);
+
+                    expect(actual).toEqual([expected]);
+                });
+        }
+    );
 });
