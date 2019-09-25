e960d2b63ee009a99c409245cfef2dc7abe5edf1
Sebastien Gerard <sgerard@emasphere.com>
2019-09-25 21:50:35 +0200

    props

--- server/src/main/java/be/sgerard/i18n/service/i18n/file/JsonICUTranslationBundleHandler.java
+++ server/src/main/java/be/sgerard/i18n/service/i18n/file/JsonICUTranslationBundleHandler.java
@@ -5,14 +5,24 @@ import be.sgerard.i18n.model.i18n.BundleType;
 import be.sgerard.i18n.model.i18n.file.ScannedBundleFileDto;
 import be.sgerard.i18n.model.i18n.file.ScannedBundleFileKeyDto;
 import be.sgerard.i18n.service.git.RepositoryAPI;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.WrappedIOException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import static be.sgerard.i18n.service.i18n.file.TranslationFileUtils.mapToNullIfEmpty;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.singletonMap;
+import static java.util.stream.Collectors.*;
+import static org.springframework.util.StringUtils.isEmpty;
 
 /**
  * @author Sebastien Gerard
@@ -20,12 +30,21 @@ import java.util.stream.Stream;
 @Component
 public class JsonICUTranslationBundleHandler implements TranslationBundleHandler {
 
+    private static final Pattern BUNDLE_PATTERN = Pattern.compile("^(.*)\\.json$");
+
     private final List<String> pathToIgnores;
     private final AntPathMatcher antPathMatcher;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public JsonICUTranslationBundleHandler(AppProperties appProperties, ObjectMapper objectMapper) {
+        this.pathToIgnores = appProperties.getJsonIcuTranslationBundleDirsAsList();
+        this.objectMapper = objectMapper;
+        this.antPathMatcher = new AntPathMatcher();
+    }
 
     public JsonICUTranslationBundleHandler(AppProperties appProperties) {
-        this.pathToIgnores = appProperties.getJsonIcuTranslationBundleDirsAsList();
-        this.antPathMatcher = new AntPathMatcher();
+        this(appProperties, new ObjectMapper());
     }
 
     @Override
@@ -40,16 +59,90 @@ public class JsonICUTranslationBundleHandler implements TranslationBundleHandler
 
     @Override
     public Stream<ScannedBundleFileDto> scanBundles(File directory, RepositoryAPI repositoryAPI) throws IOException {
-        return null;
+        return repositoryAPI.listNormalFiles(directory)
+                .map(
+                        file -> {
+                            final Matcher matcher = BUNDLE_PATTERN.matcher(file.getName());
+
+                            if (matcher.matches()) {
+                                return new ScannedBundleFileDto(
+                                        directory.getName(),
+                                        BundleType.JSON_ICU,
+                                        directory,
+                                        singletonList(Locale.forLanguageTag(matcher.group(1))),
+                                        singletonList(file)
+                                );
+                            } else {
+                                return null;
+                            }
+                        }
+                )
+                .filter(Objects::nonNull)
+                .collect(groupingBy(ScannedBundleFileDto::getName, reducing(null, ScannedBundleFileDto::merge)))
+                .values()
+                .stream();
     }
 
     @Override
     public Collection<ScannedBundleFileKeyDto> scanKeys(ScannedBundleFileDto bundleFile, RepositoryAPI repositoryAPI) throws IOException {
-        return null;
+        try {
+            return bundleFile.getFiles().stream()
+                    .flatMap(
+                            file -> {
+                                try {
+                                    return inlineValues(readValues(repositoryAPI, file))
+                                            .entrySet().stream()
+                                            .map(entry -> new ScannedBundleFileKeyDto(entry.getKey(), singletonMap(getLocale(file), mapToNullIfEmpty(entry.getValue()))));
+                                } catch (IOException e) {
+                                    throw new WrappedIOException(e);
+                                }
+                            }
+                    )
+                    .collect(groupingBy(ScannedBundleFileKeyDto::getKey, LinkedHashMap::new, reducing(null, ScannedBundleFileKeyDto::merge)))
+                    .values();
+        } catch (WrappedIOException e) {
+            throw e.getCause();
+        }
     }
 
     @Override
     public void updateBundle(ScannedBundleFileDto bundleFile, Collection<ScannedBundleFileKeyDto> keys, RepositoryAPI repositoryAPI) throws IOException {
+    }
 
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> readValues(RepositoryAPI repositoryAPI, File file) throws IOException {
+        return objectMapper.readValue(repositoryAPI.openInputStream(file), LinkedHashMap.class);
+    }
+
+    private Map<String, String> inlineValues(Map<String, Object> originalValues) {
+        return inlineValues(originalValues, "").collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @SuppressWarnings("unchecked")
+    private Stream<Map.Entry<String, String>> inlineValues(Map<String, Object> originalValues, String currentParentKey) {
+        return originalValues.entrySet().stream()
+                .flatMap(entry -> {
+                    if (entry.getValue() instanceof Map) {
+                        return inlineValues((Map<String, Object>) entry.getValue(), createKey(currentParentKey, entry.getKey()));
+                    } else if (entry.getValue() instanceof String) {
+                        return Stream.of(new AbstractMap.SimpleEntry<>(createKey(currentParentKey, entry.getKey()), mapToNullIfEmpty((String) entry.getValue())));
+                    } else {
+                        return Stream.empty();
+                    }
+                });
+    }
+
+    private String createKey(String currentParentKey, String key) {
+        return isEmpty(currentParentKey) ? key : currentParentKey + "." + key;
+    }
+
+    private Locale getLocale(File file) {
+        final Matcher matcher = BUNDLE_PATTERN.matcher(file.getName());
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("The file [" + file + "] is not a resource bundle file.");
+        }
+
+        return Locale.forLanguageTag(matcher.group(1));
     }
 }
