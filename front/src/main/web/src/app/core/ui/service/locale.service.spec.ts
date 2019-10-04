8afa5ece61949c39450b9ee3061fca60999f2a68
Sebastien Gerard <sgerard@emasphere.com>
2019-10-04 18:45:46 +0200

    fix test

--- front/src/main/web/src/app/core/ui/service/locale.service.spec.ts
+++ front/src/main/web/src/app/core/ui/service/locale.service.spec.ts
@@ -1,13 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import {getTestBed, TestBed} from '@angular/core/testing';
 
-import { LocaleService } from './locale.service';
+import {LocaleService} from './locale.service';
+import {TranslateService} from "@ngx-translate/core";
+import {UserSettingsService} from "../../../settings/service/user-settings.service";
+import {ActivatedRoute} from "@angular/router";
 
 describe('LocaleService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let injector: TestBed;
+    let service: LocaleService;
+    let translateService: TranslateService;
+    let settingsService: UserSettingsService;
+    let route: ActivatedRoute;
 
-  // TODO
-  it('should be created', () => {
-    const service: LocaleService = TestBed.get(LocaleService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: TranslateService, useValue: translateService},
+                {provide: UserSettingsService, useValue: settingsService},
+                {provide: ActivatedRoute, useValue: route}
+            ]
+        })
+
+        injector = getTestBed();
+        service = injector.get(LocaleService);
+    });
+
+    // TODO
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
