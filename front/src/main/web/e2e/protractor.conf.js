b669fa4d07f4d1f5042918257bd2f642393d32ac
Sebastien Gerard <sgerard@emasphere.com>
2019-10-09 21:47:15 +0200

    tests config

--- front/src/main/web/e2e/protractor.conf.js
+++ front/src/main/web/e2e/protractor.conf.js
@@ -29,8 +29,8 @@ exports.config = {
         browser.waitForAngularEnabled(false);
 
         await browser.get(browser.baseUrl);
-        await element(by.id('login_field')).sendKeys('sebge2-test-user');
-        await element(by.id('password')).sendKeys('SngDkVV6X9ML5cNeehWGgaf');
+        await element(by.id('login_field')).sendKeys(process.env.GIT_HUB_USER);
+        await element(by.id('password')).sendKeys(process.env.GIT_HUB_USER_PASSWORD);
         await element(by.name('commit')).click();
 
         browser.waitForAngularEnabled(true);
--- front/src/main/web/e2e/smoke-tests/test.spec.js
+++ front/src/main/web/e2e/smoke-tests/test.spec.js
@@ -1,6 +1,6 @@
 describe('Smoke Tests', function() {
 
-    it('should land to default route', async function() {
+    it('should land on default route', async function() {
         const url = await browser.getCurrentUrl();
 
         expect(url).toContain('translations');
