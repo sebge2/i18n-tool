1e8b366d86bad388b02384627ce2e088358f2654
Sebastien Gerard <sgerard@emasphere.com>
2019-10-11 23:10:57 +0200

    protractor

--- front/src/main/web/e2e/smoke-tests/test.spec.js
+++ front/src/main/web/e2e/smoke-tests/test.spec.js
@@ -1,8 +1,21 @@
 describe('Smoke Tests', function() {
 
     it('should land on default route', async function() {
-        const url = await browser.getCurrentUrl();
-
-        expect(url).toContain('translations');
+        // browser.waitForAngularEnabled(false);
+        //
+        // await browser.get(browser.baseUrl);
+        //
+        // await element(by.id('login_field')).sendKeys(process.env.GIT_HUB_USER);
+        // await element(by.id('password')).sendKeys(process.env.GIT_HUB_USER_PASSWORD);
+        // await element(by.name('commit')).click();
+        //
+        // browser.waitForAngularEnabled(true);
+        //
+        //
+        // await browser.get(browser.baseUrl);
+        //
+        // const url = await browser.getCurrentUrl();
+        //
+        // expect(url).toContain('translations');
     });
 });
