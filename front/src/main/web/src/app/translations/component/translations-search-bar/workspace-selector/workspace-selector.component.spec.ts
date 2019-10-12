56c6ad90f19a267b856b838205e4581020e38b76
Sebastien Gerard <sgerard@emasphere.com>
2019-10-12 10:52:10 +0200

    tests

--- front/src/main/web/src/app/translations/component/translations-search-bar/workspace-selector/workspace-selector.component.spec.ts
+++ front/src/main/web/src/app/translations/component/translations-search-bar/workspace-selector/workspace-selector.component.spec.ts
@@ -7,33 +7,47 @@ import {HttpClientModule} from "@angular/common/http";
 import {CoreEventModule} from "../../../../core/event/core-event.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CoreUiModule} from "../../../../core/ui/core-ui.module";
+import {WorkspaceService} from "../../../service/workspace.service";
+import {BehaviorSubject} from "rxjs";
+import {Workspace} from "../../../model/workspace.model";
 
 describe('WorkspaceSelectorComponent', () => {
     let component: WorkspaceSelectorComponent;
     let fixture: ComponentFixture<WorkspaceSelectorComponent>;
+    let workspaceService: WorkspaceService;
+    let workspaces: BehaviorSubject<Workspace[]>;
 
     beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                BrowserAnimationsModule,
-                CoreSharedModule,
-                CoreUiModule,
-                CoreEventModule,
-                HttpClientModule,
-                TranslateModule.forRoot()
-            ],
-            declarations: [WorkspaceSelectorComponent]
-        })
-            .compileComponents();
-    }));
+        workspaceService = jasmine.createSpyObj('workspaceService', ['getWorkspaces']);
 
-    beforeEach(() => {
+        workspaces = new BehaviorSubject([]);
+
+        workspaceService.getWorkspaces = jasmine.createSpy().and.returnValue(workspaces);
+
+        TestBed
+            .configureTestingModule({
+                imports: [
+                    BrowserAnimationsModule,
+                    CoreSharedModule,
+                    CoreUiModule,
+                    CoreEventModule,
+                    HttpClientModule,
+                    TranslateModule.forRoot()
+                ],
+                providers: [
+                    {provide: WorkspaceService, useValue: workspaceService}
+                ],
+                declarations: [WorkspaceSelectorComponent]
+            })
+            .compileComponents();
+
         fixture = TestBed.createComponent(WorkspaceSelectorComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    }));
 
     it('should create', () => {
+        fixture.detectChanges();
+
         expect(component).toBeTruthy(); // TODO
     });
 });
