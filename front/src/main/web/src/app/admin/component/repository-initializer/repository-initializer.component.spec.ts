4c596542a51850d7e3be1bc521bf08491a3e423d
Sebastien Gerard <sgerard@emasphere.com>
2019-10-12 10:25:02 +0200

    tests

--- front/src/main/web/src/app/admin/component/repository-initializer/repository-initializer.component.spec.ts
+++ front/src/main/web/src/app/admin/component/repository-initializer/repository-initializer.component.spec.ts
@@ -1,36 +1,32 @@
-import {ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {RepositoryInitializerComponent} from './repository-initializer.component';
 import {TranslateModule} from "@ngx-translate/core";
 import {RepositoryService} from "../../../translations/service/repository.service";
 import {CoreSharedModule} from "../../../core/shared/core-shared-module";
 import {CoreUiModule} from "../../../core/ui/core-ui.module";
-import {BehaviorSubject, Observable, Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 import {Repository} from "../../../translations/model/repository.model";
 import {RepositoryStatus} from "../../../translations/model/repository-status.model";
 import {By} from "@angular/platform-browser";
 
 describe('RepositoryInitializerComponent', () => {
-    let injector: TestBed;
     let component: RepositoryInitializerComponent;
     let fixture: ComponentFixture<RepositoryInitializerComponent>;
-    let repositoryService: MockRepositoryService;
-
-    class MockRepositoryService {
-
-        readonly subject: Subject<Repository> = new BehaviorSubject(new Repository(<Repository>{status: RepositoryStatus.NOT_INITIALIZED}));
-
-        public getRepository(): Observable<Repository> {
-            return this.subject;
-        }
-
-        public initialize(): Promise<any> {
-            return Promise.resolve();
-        }
-    }
+    let repositoryService: RepositoryService;
+    let repository: BehaviorSubject<Repository>;
 
     beforeEach((() => {
-        repositoryService = new MockRepositoryService();
+        repositoryService = jasmine.createSpyObj('repositoryService', ['getRepository', 'initialize']);
+
+        repository = new BehaviorSubject(
+            new Repository(<Repository>{
+                status: RepositoryStatus.NOT_INITIALIZED
+            })
+        );
+
+        repositoryService.getRepository = jasmine.createSpy().and.returnValue(repository);
+        repositoryService.initialize = jasmine.createSpy().and.returnValue(Promise.resolve());
 
         TestBed
             .configureTestingModule({
@@ -46,8 +42,6 @@ describe('RepositoryInitializerComponent', () => {
             })
             .compileComponents();
 
-        injector = getTestBed();
-
         fixture = TestBed.createComponent(RepositoryInitializerComponent);
         component = fixture.componentInstance;
     }));
@@ -62,8 +56,6 @@ describe('RepositoryInitializerComponent', () => {
 
     it('should display button',
         () => {
-            spyOn(repositoryService, 'initialize').and.returnValue(Promise.resolve());
-
             fixture.detectChanges();
 
             const button = fixture.debugElement.nativeElement.querySelector('#button');
@@ -78,7 +70,7 @@ describe('RepositoryInitializerComponent', () => {
 
     it('should display initializing',
         async () => {
-            repositoryService.subject.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZING}));
+            repository.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZING}));
 
             fixture.detectChanges();
             fixture.whenStable().then(() => {
@@ -89,8 +81,8 @@ describe('RepositoryInitializerComponent', () => {
 
     it('should display initialized',
         async () => {
-            repositoryService.subject.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZING}));
-            repositoryService.subject.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZED}));
+            repository.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZING}));
+            repository.next(new Repository(<Repository>{status: RepositoryStatus.INITIALIZED}));
 
             fixture.detectChanges();
             fixture.whenStable().then(() => {
