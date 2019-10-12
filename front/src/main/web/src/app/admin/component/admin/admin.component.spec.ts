9daa9c015b7fa52e1bd3ecda8f15ca8ed2e3b80d
Sebastien Gerard <sgerard@emasphere.com>
2019-10-12 08:39:45 +0200

    tests

--- front/src/main/web/src/app/admin/component/admin/admin.component.spec.ts
+++ front/src/main/web/src/app/admin/component/admin/admin.component.spec.ts
@@ -8,36 +8,70 @@ import {CoreSharedModule} from "../../../core/shared/core-shared-module";
 import {HttpClientModule} from "@angular/common/http";
 import {CoreEventModule} from "../../../core/event/core-event.module";
 import {CoreUiModule} from "../../../core/ui/core-ui.module";
+import {RepositoryService} from "../../../translations/service/repository.service";
+import {BehaviorSubject} from "rxjs";
+import {WorkspaceService} from "../../../translations/service/workspace.service";
+import {Repository} from "../../../translations/model/repository.model";
+import {RepositoryStatus} from "../../../translations/model/repository-status.model";
+import {Workspace} from "../../../translations/model/workspace.model";
 
 describe('AdminComponent', () => {
     let component: AdminComponent;
     let fixture: ComponentFixture<AdminComponent>;
+    let repositoryService: RepositoryService;
+    let workspaceService: WorkspaceService;
+
+    let workspaces: BehaviorSubject<Workspace[]>;
+    let repository: BehaviorSubject<Repository>;
 
     beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                CoreUiModule,
-                CoreSharedModule,
-                CoreEventModule,
-                HttpClientModule,
-                TranslateModule.forRoot()
-            ],
-            declarations: [
-                AdminComponent,
-                RepositoryInitializerComponent,
-                WorkspaceTableComponent
-            ]
-        })
-            .compileComponents();
-    }));
+        repositoryService = jasmine.createSpyObj('repositoryService', ['getRepository']);
+        workspaceService = jasmine.createSpyObj('workspaceService', ['getWorkspaces']);
 
-    beforeEach(() => {
+        workspaces = new BehaviorSubject([]);
+        repository = new BehaviorSubject(
+            new Repository(<Repository>{
+                status: RepositoryStatus.NOT_INITIALIZED
+            })
+        );
+
+        workspaceService.getWorkspaces = jasmine.createSpy().and.returnValue(workspaces);
+        repositoryService.getRepository = jasmine.createSpy().and.returnValue(repository);
+
+        TestBed
+            .configureTestingModule({
+                imports: [
+                    CoreUiModule,
+                    CoreSharedModule,
+                    CoreEventModule,
+                    HttpClientModule,
+                    TranslateModule.forRoot()
+                ],
+                declarations: [
+                    AdminComponent,
+                    RepositoryInitializerComponent,
+                    WorkspaceTableComponent
+                ],
+                providers: [
+                    {provide: RepositoryService, useValue: repositoryService},
+                    {provide: WorkspaceService, useValue: workspaceService}
+                ],
+            })
+            .compileComponents();
+
         fixture = TestBed.createComponent(AdminComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    }));
 
-    it('should create', () => {
+    it('should display everything ok', () => {
+        repository.next(
+            new Repository(<Repository>{
+                status: RepositoryStatus.INITIALIZED
+            })
+        );
+
+        fixture.detectChanges();
+
         expect(component).toBeTruthy(); // TODO
     });
 });
