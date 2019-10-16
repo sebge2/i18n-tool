d0368976c5c6fe931c42397f9f5f5be3b97d7d21
Sebastien Gerard <sgerard@emasphere.com>
2019-10-16 20:14:34 +0200

    security

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 # I18n Tool
 Tool scanning translations in a GitHub repository. A UI editor allows to find missing translations and fill them.
+
+
+## Backend Endpoints
+
+* /api/*
+* /ws/*
+* /auth/*
+    * /auth/login-password
+    * /auth/oauth2/authorize-client/{client-registration-id}
+    * /auth/oauth2/code/{code}
+    
+## Angular Routes
+
+* /login
+* /logout/success
--- front/src/main/web/src/app/app-routing.module.ts
+++ front/src/main/web/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: '/translations'},
     {path: '', loadChildren: () => import('./core/ui/core-ui.module').then(m => m.CoreUiModule)},
+    {path: '', loadChildren: () => import('./core/auth/core-auth.module').then(m => m.CoreAuthModule)},
     {path: 'error', loadChildren: () => import('./error/core-error.module').then(m => m.CoreErrorModule)},
-    {path: 'logout', loadChildren: () => import('./core/auth/core-auth.module').then(m => m.CoreAuthModule)},
     {path: '**', redirectTo: "/error/404"}
 ];
 
