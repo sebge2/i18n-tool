fc946521b9fc179bb9922912608c3bc79281d1bd
Sebastien Gerard <sgerard@emasphere.com>
2019-10-19 21:03:18 +0200

    service

--- front/src/main/web/src/app/core/auth/service/authentication.service.ts
+++ front/src/main/web/src/app/core/auth/service/authentication.service.ts
@@ -1,19 +1,126 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Injectable, OnDestroy} from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Observable, of, Subject} from "rxjs";
 import {User} from "../model/user.model";
-import {map} from "rxjs/operators";
+import {catchError, map, tap} from "rxjs/operators";
+import {NotificationService} from "../../notification/service/notification.service";
+import {AuthenticationErrorType} from "../model/authentication-error-type.model";
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService implements OnDestroy {
 
-    constructor(private httpClient: HttpClient) {
+    private _user: Subject<User> = new Subject<User>();
+    private destroy$ = new Subject();
+
+    constructor(private httpClient: HttpClient,
+                private notificationService: NotificationService) {
+        this.httpClient.get<User>('/api/authentication/user')
+            .pipe(map(user => new User(user)))
+            .subscribe(
+                user => {
+                    console.debug('There is a current user, send next user.', user);
+                    this._user.next(user)
+                },
+                (result: HttpResponse<any>) => {
+                    if (result.status != 404) {
+                        console.error('Error while retrieving current user.', result);
+                        this.notificationService.displayErrorMessage("Error while retrieving current user.");
+                    } else {
+                        console.debug('There is no current user, send next user null.');
+                    }
+
+                    this._user.next(null);
+                }
+            );
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.complete();
     }
 
     get currentUser(): Observable<User> {
-        return this.httpClient.get<User>('/api/authentication/user')
-            .pipe(map(user => new User(user)));
+        return this._user;
+    }
+
+    authenticateWithUserPassword(username: string, password: string): Observable<User> {
+        return this.httpClient
+            .get(
+                "/api/authentication/user",
+                {
+                    headers: {
+                        'Authorization': 'Basic ' + btoa(username + ':' + password),
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }
+                }
+            )
+            .pipe(
+                map(
+                    (user: User) => {
+                        return new User(user);
+                    }
+                ),
+                catchError((result: HttpResponse<any>) => {
+                        if (result.status == 401) {
+                            return of(AuthenticationErrorType.WRONG_CREDENTIALS);
+                        } else {
+                            console.error('Error while authenticating user.', result);
+                            this.notificationService.displayErrorMessage('Error while authenticating user.');
+
+                            return of(AuthenticationErrorType.AUTHENTICATION_SYSTEM_ERROR);
+                        }
+                    }
+                ),
+                tap(
+                    (value: any) => {
+                        if (value instanceof User) {
+                            console.log('Authentication succeeded, send next user.', value);
+
+                            this._user.next(<User>value);
+                        }
+                    }
+                )
+            );
+    }
+
+    authenticateWithGitHubAuthKey(authKey: string): Observable<User> {
+        return this.httpClient
+            .get(
+                "/api/authentication/user",
+                {
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('#' + authKey + ':'),
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }
+                }
+            )
+            .pipe(
+                map(
+                    (user: User) => {
+                        return new User(user);
+                    }
+                ),
+                catchError((result: HttpResponse<any>) => {
+                        if (result.status == 401) {
+                            return of(AuthenticationErrorType.WRONG_CREDENTIALS);
+                        } else {
+                            console.error('Error while authenticating user.', result);
+                            this.notificationService.displayErrorMessage('Error while authenticating user.');
+
+                            return of(AuthenticationErrorType.AUTHENTICATION_SYSTEM_ERROR);
+                        }
+                    }
+                ),
+                tap(
+                    (value: any) => {
+                        if (value instanceof User) {
+                            console.log('Authentication succeeded, send next user.', value);
+
+                            this._user.next(<User>value);
+                        }
+                    }
+                )
+            );
     }
 }
