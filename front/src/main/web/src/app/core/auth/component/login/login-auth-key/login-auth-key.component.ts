0103e685638b0174dfac904b3475c2b1c3dfedac
Sebastien Gerard <sgerard@emasphere.com>
2019-10-21 07:27:08 +0200

    checks

---
+++ front/src/main/web/src/app/core/auth/component/login/login-auth-key/login-auth-key.component.ts
@@ -0,0 +1,86 @@
+import {Component, OnInit} from '@angular/core';
+import {MatProgressButtonOptions} from 'mat-progress-buttons';
+import {AuthenticationService} from "../../../service/authentication.service";
+import {User} from "../../../model/user.model";
+import {AuthenticationErrorType} from "../../../model/authentication-error-type.model";
+import {Router} from "@angular/router";
+import {NotificationService} from "../../../../notification/service/notification.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+    selector: 'app-login-auth-key',
+    templateUrl: './login-auth-key.component.html',
+    styleUrls: ['./login-auth-key.component.css']
+})
+export class LoginAuthKeyComponent implements OnInit {
+
+    buttonOptions: MatProgressButtonOptions = {
+        active: false,
+        text: 'Login',
+        spinnerSize: 18,
+        raised: true,
+        stroked: false,
+        buttonColor: 'primary',
+        spinnerColor: 'accent',
+        fullWidth: false,
+        disabled: false,
+        mode: 'indeterminate',
+        buttonIcon: {
+            fontIcon: 'send'
+        }
+    };
+
+    form: FormGroup;
+
+    constructor(private authenticationService: AuthenticationService,
+                private notificationService: NotificationService,
+                private formBuilder: FormBuilder,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            authkey: ['', Validators.required]
+        });
+    }
+
+    public login(): void {
+        this.buttonOptions.active = true;
+
+        if (this.form.errors && this.form.errors['authFailed']) {
+            delete this.form.errors['authFailed'];
+        }
+
+        this.authenticationService.authenticateWithGitHubAuthKey(this.form.get('authkey').value)
+            .toPromise()
+            .then(
+                (user: User) => {
+                    this.router.navigate(['/translations']);
+                }
+            )
+            .catch(
+                (errorType: AuthenticationErrorType) => {
+                    if (errorType == AuthenticationErrorType.WRONG_CREDENTIALS) {
+                        this.form.setErrors({'authFailed': true});
+                    }
+                }
+            )
+            .finally(
+                () => {
+                    this.buttonOptions.active = false;
+                }
+            );
+    }
+
+    paste() {
+        navigator.clipboard.readText()
+            .then(text => {
+                this.form.get('authkey').setValue(text);
+            })
+            .catch(err => {
+                console.error('Failed to read clipboard contents.', err);
+
+                this.notificationService.displayErrorMessage('Failed to read clipboard contents.');
+            });
+    }
+}
--- front/src/main/web/src/app/core/auth/core-auth.module.ts
+++ front/src/main/web/src/app/core/auth/core-auth.module.ts
@@ -4,19 +4,20 @@ import {RouterModule, Routes} from "@angular/router";
 import {LogoutComponent} from './component/logout/logout.component';
 import {LoginComponent} from './component/login/login.component';
 import {CoreUiModule} from "../ui/core-ui.module";
-import { LoginUserPasswordComponent } from './component/login/login-user-password/login-user-password.component';
+import {LoginUserPasswordComponent} from './component/login/login-user-password/login-user-password.component';
 import {CoreSharedModule} from "../shared/core-shared-module";
 import {FormsModule} from "@angular/forms";
-import { LoginAuthKeyComponent } from './component/login/login-auth-key/login-auth-key.component';
-import { LoginProviderComponent } from './component/login/login-provider/login-provider.component';
+import {LoginAuthKeyComponent} from './component/login/login-auth-key/login-auth-key.component';
+import {LoginProviderComponent} from './component/login/login-provider/login-provider.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {LoginGuard} from "./service/guard/login.guard";
+import {LogoutGuard} from "./service/guard/logout.guard";
 
 const appRoutes: Routes = [
     {
         path: 'logout',
-        children: [
-            {path: 'success', component: LogoutComponent}
-        ]
+        component: LogoutComponent,
+        canActivate: [LogoutGuard]
     },
     {
         path: 'login',
