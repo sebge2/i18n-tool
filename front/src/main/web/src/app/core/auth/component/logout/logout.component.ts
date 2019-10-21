fe95bfb004c64961badd3d12e8a522d33c902fc6
Sebastien Gerard <sgerard@emasphere.com>
2019-10-21 07:47:57 +0200

    checks

--- front/src/main/web/src/app/core/auth/component/logout/logout.component.ts
+++ front/src/main/web/src/app/core/auth/component/logout/logout.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../service/authentication.service";
 
 @Component({
-  selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+    selector: 'app-logout',
+    templateUrl: './logout.component.html',
+    styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+    private loggedOut: boolean = null;
 
-  ngOnInit() {
-  }
+    constructor(private authenticationService: AuthenticationService) {
+    }
+
+    ngOnInit() {
+        this.authenticationService
+            .logout()
+            .subscribe(
+                (result: any) => {
+                    this.loggedOut = true;
+                },
+                (error: any) => {
+                    this.loggedOut = false;
+                }
+            )
+    }
 
 }
