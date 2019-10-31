c1802e4f5548c5071281cdb1368ca10a58329102
Sebastien Gerard <sgerard@emasphere.com>
2019-10-31 21:02:36 +0100

    auth

---
+++ front/src/main/web/src/app/core/auth/service/user.service.ts
@@ -0,0 +1,68 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import {BehaviorSubject, Observable, Subject} from "rxjs";
+import {User} from "../model/user.model";
+import {HttpClient} from "@angular/common/http";
+import {NotificationService} from "../../notification/service/notification.service";
+import {Events} from "../../event/model/events.model";
+import {takeUntil} from "rxjs/operators";
+import {EventService} from "../../event/service/event.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService implements OnDestroy {
+
+    private _users: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+    private destroy$ = new Subject();
+
+    constructor(private httpClient: HttpClient,
+                private eventService: EventService,
+                private notificationService: NotificationService) {
+        this.httpClient.get<User[]>('/api/user').toPromise()
+            .then(users => this._users.next(users.map(user => new User(user))))
+            .catch(reason => {
+                console.error("Error while retrieving users.", reason);
+                this.notificationService.displayErrorMessage("Error while retrieving users.")
+            });
+
+        this.eventService.subscribe(Events.UPDATED_USER, User)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (user: User) => {
+                    const users = this._users.getValue().slice();
+
+                    const index = users.findIndex(current => user.id === current.id);
+                    if (index >= 0) {
+                        users[index] = user;
+                    } else {
+                        users.push(user);
+                    }
+
+                    this._users.next(users);
+                }
+            );
+
+        this.eventService.subscribe(Events.DELETED_USER, User)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(
+                (user: User) => {
+                    const users = this._users.getValue().slice();
+
+                    const index = users.findIndex(current => user.id === current.id);
+                    if (index >= 0) {
+                        users.splice(index, 1);
+                    }
+
+                    this._users.next(users);
+                }
+            );
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.complete();
+    }
+
+    getUsers(): Observable<User[]> {
+        return this._users;
+    }
+}
