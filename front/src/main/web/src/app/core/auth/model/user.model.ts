5d4aeb53b85f842ef57b8957a9c45b3dafbe417c
Sebastien Gerard <sgerard@emasphere.com>
2019-11-01 16:59:11 +0100

    user edit

--- front/src/main/web/src/app/core/auth/model/user.model.ts
+++ front/src/main/web/src/app/core/auth/model/user.model.ts
@@ -7,17 +7,35 @@ export class User {
     readonly email: string;
     readonly avatarUrl: string;
     readonly roles: UserRole[];
+    readonly type: UserType;
 
     constructor(user: User = <User>{}) {
         Object.assign(this, user);
 
-        this.roles =
-            (user.roles != null)
-                ? user.roles
-                : [];
+        this.roles = (user.roles != null) ? user.roles : [];
     }
 
     hasRole(role: UserRole): boolean {
         return this.roles.includes(role);
     }
+
+    isInternal(): boolean {
+        return this.type == UserType.INTERNAL;
+    }
+
+    isExternal(): boolean {
+        return this.type == UserType.EXTERNAL;
+    }
+
+    hasAdminRole(): boolean {
+        return this.hasRole(UserRole.ADMIN);
+    }
+}
+
+export enum UserType {
+
+    EXTERNAL = "EXTERNAL",
+
+    INTERNAL = "INTERNAL"
+
 }
