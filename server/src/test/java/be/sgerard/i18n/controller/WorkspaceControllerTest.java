975fe1158550980be1f50b158273c1ea4b424b80
Sebastien Gerard <sgerard@emasphere.com>
2020-06-10 16:20:56 +0200

    reactive

--- server/src/test/java/be/sgerard/i18n/controller/WorkspaceControllerTest.java
+++ server/src/test/java/be/sgerard/i18n/controller/WorkspaceControllerTest.java
@@ -3,15 +3,12 @@ package be.sgerard.i18n.controller;
 import be.sgerard.i18n.model.repository.dto.GitHubRepositoryDto;
 import be.sgerard.i18n.model.repository.dto.GitRepositoryDto;
 import be.sgerard.i18n.model.workspace.WorkspaceDto;
-import be.sgerard.test.i18n.support.GitHubTest;
 import be.sgerard.test.i18n.support.WithInternalUser;
-import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.transaction.annotation.Transactional;
 
 import static be.sgerard.test.i18n.model.GitRepositoryCreationDtoTestUtils.i18nToolLocalRepositoryCreationDto;
 import static be.sgerard.test.i18n.model.GitRepositoryCreationDtoTestUtils.i18nToolRepositoryCreationDto;
-import static be.sgerard.test.i18n.model.UserDtoTestUtils.userJohnDoeCreation;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,79 +18,102 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Sebastien Gerard
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class WorkspaceControllerTest extends AbstractControllerTest {
 
-    @Before
-    public void setup() throws Exception {
-        user.createUser(userJohnDoeCreation().build());
+    @BeforeAll
+    public void setupRepo() throws Exception {
+        gitRepo
+                .createMockFor(i18nToolRepositoryCreationDto())
+                .allowAnonymousRead()
+                .basedOnCurrentProject()
+                .create();
+        gitRepo
+                .createMockFor(i18nToolLocalRepositoryCreationDto())
+                .allowAnonymousRead()
+                .basedOnCurrentProject()
+                .create();
     }
 
-    @Test
-    @Transactional
-    @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
-    @GitHubTest
-    public void synchronizeGitHubRepository() throws Exception {
-        final GitHubRepositoryDto repository = this.repository.create(i18nToolRepositoryCreationDto(), GitHubRepositoryDto.class).initialize().get();
-
-        asyncMvc
-                .perform(post("/api/repository/{id}/workspace/do?action=SYNCHRONIZE", repository.getId()))
-                .andExpectStarted()
-                .andWaitResult()
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(1))));
+    @AfterAll
+    public void destroy() {
+        gitRepo.destroy();
     }
 
-    @Test
-    @Transactional
-    @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
-    public void synchronizeGitRepository() throws Exception {
-        final GitRepositoryDto repository = this.repository.create(i18nToolLocalRepositoryCreationDto(), GitRepositoryDto.class).initialize().get();
+    @Nested
+    @DisplayName("GitHub")
+    class GitHub extends AbstractControllerTest {
 
-        asyncMvc
-                .perform(post("/api/repository/{id}/workspace/do?action=SYNCHRONIZE", repository.getId()))
-                .andExpectStarted()
-                .andWaitResult()
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(1))));
+        @Test
+        @Transactional
+        @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
+        public void synchronize() throws Exception {
+            final GitHubRepositoryDto repository = this.repository.create(i18nToolRepositoryCreationDto(), GitHubRepositoryDto.class).initialize().get();
+
+            asyncMvc
+                    .perform(post("/api/repository/{id}/workspace/do?action=SYNCHRONIZE", repository.getId()))
+                    .andExpectStarted()
+                    .andWaitResult()
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(1))));
+        }
+
+        @Test
+        @Transactional
+        @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
+        public void initialize() throws Exception {
+            final WorkspaceDto masterWorkspace = repository
+                    .create(i18nToolRepositoryCreationDto(), GitHubRepositoryDto.class)
+                    .initialize()
+                    .workspaces()
+                    .sync()
+                    .getOrDie("master");
+
+            asyncMvc
+                    .perform(post("/api/workspace/{id}/do?action=INITIALIZE", masterWorkspace.getId()))
+                    .andExpectStarted()
+                    .andWaitResult()
+                    .andExpect(status().isOk());
+        }
     }
 
-    @Test
-    @Transactional
-    @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
-    @GitHubTest
-    public void initializeGitHubRepository() throws Exception {
-        final WorkspaceDto masterWorkspace = repository
-                .create(i18nToolRepositoryCreationDto(), GitHubRepositoryDto.class)
-                .initialize()
-                .workspaces()
-                .sync()
-                .getOrDie("master");
+    @Nested
+    @DisplayName("Git")
+    class Git extends AbstractControllerTest {
 
-        asyncMvc
-                .perform(post("/api/workspace/{id}/do?action=INITIALIZE", masterWorkspace.getId()))
-                .andExpectStarted()
-                .andWaitResult()
-                .andExpect(status().isOk());
+        @Test
+        @Transactional
+        @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
+        public void synchronize() throws Exception {
+            final GitRepositoryDto repository = this.repository.create(i18nToolLocalRepositoryCreationDto(), GitRepositoryDto.class).initialize().get();
+
+            asyncMvc
+                    .perform(post("/api/repository/{id}/workspace/do?action=SYNCHRONIZE", repository.getId()))
+                    .andExpectStarted()
+                    .andWaitResult()
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(1))));
+        }
+
+        @Test
+        @Transactional
+        @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
+        public void initialize() throws Exception {
+            final WorkspaceDto masterWorkspace = repository
+                    .create(i18nToolLocalRepositoryCreationDto(), GitRepositoryDto.class)
+                    .initialize()
+                    .workspaces()
+                    .sync()
+                    .getOrDie("master");
+
+            asyncMvc
+                    .perform(post("/api/workspace/{id}/do?action=INITIALIZE", masterWorkspace.getId()))
+                    .andExpectStarted()
+                    .andWaitResult()
+                    .andExpect(status().isOk());
+        }
     }
 
-    @Test
-    @Transactional
-    @WithInternalUser(roles = {"MEMBER_OF_ORGANIZATION", "ADMIN"})
-    @GitHubTest
-    public void initializeGitRepository() throws Exception {
-        final WorkspaceDto masterWorkspace = repository
-                .create(i18nToolLocalRepositoryCreationDto(), GitRepositoryDto.class)
-                .initialize()
-                .workspaces()
-                .sync()
-                .getOrDie("master");
-
-        asyncMvc
-                .perform(post("/api/workspace/{id}/do?action=INITIALIZE", masterWorkspace.getId()))
-                .andExpectStarted()
-                .andWaitResult()
-                .andExpect(status().isOk());
-    }
 
     // TODO findall
     // TODO findall of repo
