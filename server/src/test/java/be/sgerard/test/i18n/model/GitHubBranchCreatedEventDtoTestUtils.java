2721dd0971457ea7d5cecc0d908d6374214785a7
Sebastien Gerard <sgerard@emasphere.com>
2020-06-19 22:45:58 +0200

    translations

--- server/src/test/java/be/sgerard/test/i18n/model/GitHubBranchCreatedEventDtoTestUtils.java
+++ server/src/test/java/be/sgerard/test/i18n/model/GitHubBranchCreatedEventDtoTestUtils.java
@@ -11,11 +11,11 @@ public final class GitHubBranchCreatedEventDtoTestUtils {
     private GitHubBranchCreatedEventDtoTestUtils() {
     }
 
-    public static GitHubBranchCreatedEventDto i18nToolRelease20204BranchCreatedEvent() {
+    public static GitHubBranchCreatedEventDto i18nToolRelease20205BranchCreatedEvent() {
         return new GitHubBranchCreatedEventDto(
                 new BaseGitHubWebHookEventDto.Repository("048650d2-7a90-4086-9315-2960acf71099", "sebge2/i18n-tool"),
                 "branch",
-                "release/2020.4"
+                "release/2020.5"
         );
     }
 }
