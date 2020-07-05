86b7746d686008fb217c41976fbe311a9edaceca
Sebastien Gerard <sgerard@emasphere.com>
2020-07-05 15:25:39 +0200

    oauth

--- server/src/main/java/be/sgerard/i18n/configuration/SecurityConfiguration.java
+++ server/src/main/java/be/sgerard/i18n/configuration/SecurityConfiguration.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.DelegatingReactiveAuthenticationManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2LoginReactiveAuthenticationManager;
@@ -21,17 +21,21 @@ import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuth
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultReactiveOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.session.data.mongo.config.annotation.web.reactive.EnableMongoWebSession;
 import reactor.core.publisher.Mono;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -76,22 +80,24 @@ public class SecurityConfiguration {
     @Autowired
     ReactiveClientRegistrationRepository clientRegistrationRepository;
 
-//    @Bean
-//    public ReactiveOAuth2UserService reactiveOAuth2UserService() {
-//        return new ReactiveOAuth2UserService() {
-//
-//            @Override
-//            public Mono loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//                System.out.println("SGE");
-//                System.out.println("");
-//                System.out.println("");
-//                System.out.println("");
-//                System.out.println("");
-//
-//                return Mono.empty();
-//            }
-//        };
-//    }
+    @Bean
+    public ReactiveOAuth2UserService reactiveOAuth2UserService() {
+        return new DefaultReactiveOAuth2UserService() {
+
+            @Override
+            public Mono<OAuth2User> loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+                return super.loadUser(userRequest)
+                        .map(user -> {
+                            final HashSet<GrantedAuthority> authorities = new HashSet<>();
+                            authorities.addAll(user.getAuthorities());
+                            authorities.add(UserRole.MEMBER_OF_ORGANIZATION.toAuthority());
+                            authorities.add(UserRole.ADMIN.toAuthority());
+
+                            return new DefaultOAuth2User(authorities, user.getAttributes(), "id");
+                        });
+            }
+        };
+    }
 
     @Bean
     public OAuth2LoginReactiveAuthenticationManager manager() {
@@ -125,14 +131,11 @@ public class SecurityConfiguration {
 
                 .securityContextRepository(new WebSessionServerSecurityContextRepository())
 
-
-                .oauth2Login(new Customizer<ServerHttpSecurity.OAuth2LoginSpec>() {
-                    @Override
-                    public void customize(ServerHttpSecurity.OAuth2LoginSpec oAuth2LoginSpec) {
+                .oauth2Login(oAuth2LoginSpec ->
                         oAuth2LoginSpec
-                                .authorizationRequestResolver(new DefaultServerOAuth2AuthorizationRequestResolver(repository, ServerWebExchangeMatchers.pathMatchers(("/auth/oauth2/authorize-client/{registrationId}"))));
-                    }
-                })
+                                .authorizationRequestResolver(new DefaultServerOAuth2AuthorizationRequestResolver(repository, ServerWebExchangeMatchers.pathMatchers(("/auth/oauth2/authorize-client/{registrationId}"))))
+                                .authenticationMatcher(new PathPatternParserServerWebExchangeMatcher("/auth/oauth2/code/{registrationId}"))
+                )
 
                 // TODO login
 
