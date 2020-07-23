2acda50f721abf847cd7441d9fb333c11991baa0
Sebastien Gerard <sgerard@emasphere.com>
2020-07-23 21:27:32 +0200

    button

---
+++ front/src/main/web/src/app/core/shared/component/form-save-button/form-save-button.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-form-save-button',
+  templateUrl: './form-save-button.component.html',
+  styleUrls: ['./form-save-button.component.css']
+})
+export class FormSaveButtonComponent implements OnInit {
+
+  @Input() public form: FormGroup;
+  @Input() public loading: boolean;
+  @Output() public save = new EventEmitter<void>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  public onSave() {
+    this.save.emit();
+  }
+
+}
