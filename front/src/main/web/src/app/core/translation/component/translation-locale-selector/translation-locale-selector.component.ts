0ae80856b46d12728af66036179282aa4501b534
Sebastien Gerard <sgerard@emasphere.com>
2020-07-23 06:51:55 +0200

    repo

--- front/src/main/web/src/app/core/translation/component/translation-locale-selector/translation-locale-selector.component.ts
+++ front/src/main/web/src/app/core/translation/component/translation-locale-selector/translation-locale-selector.component.ts
@@ -1,14 +1,28 @@
-import {Component, ElementRef, EventEmitter, forwardRef, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {BehaviorSubject, combineLatest, Observable, Subject} from "rxjs";
-import {ControlValueAccessor, DefaultValueAccessor, FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {
+    AfterViewInit,
+    Component,
+    DoCheck,
+    ElementRef,
+    forwardRef,
+    HostBinding,
+    Injector,
+    Input,
+    OnDestroy,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+import {combineLatest, Observable, Subject} from "rxjs";
+import {ControlValueAccessor, FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR, NgControl} from "@angular/forms";
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {TranslationLocaleService} from "../../../../translations/service/translation-locale.service";
 import {MatChipInputEvent} from "@angular/material/chips";
-import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
+import {MatAutocomplete, MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 import {TranslationLocale} from "../../../../translations/model/translation-locale.model";
 import {map, startWith, takeUntil} from "rxjs/operators";
 import * as _ from "lodash";
 import {MatFormFieldControl} from "@angular/material/form-field";
+import {coerceBooleanProperty} from "@angular/cdk/coercion";
+import {FocusMonitor} from '@angular/cdk/a11y';
 
 @Component({
     selector: 'app-translation-locale-selector',
@@ -20,20 +34,28 @@ import {MatFormFieldControl} from "@angular/material/form-field";
             useExisting: forwardRef(() => TranslationLocaleSelectorComponent),
             multi: true
         },
-        // {provide: MatFormFieldControl, useExisting: TranslationLocaleSelectorComponent}
-    ]
+        {provide: MatFormFieldControl, useExisting: TranslationLocaleSelectorComponent}
+    ],
 })
-export class TranslationLocaleSelectorComponent implements OnInit/*, ControlValueAccessor*/ {
-
-    @Input() public value: TranslationLocale[] = [];
-    @Output() public readonly valueChange: EventEmitter<TranslationLocale[]> = new EventEmitter();
+// https://itnext.io/creating-a-custom-form-field-control-compatible-with-reactive-forms-and-angular-material-cf195905b451
+// https://material.angular.io/guide/creating-a-custom-form-field-control
+export class TranslationLocaleSelectorComponent implements OnInit, OnDestroy, AfterViewInit, DoCheck, ControlValueAccessor, MatFormFieldControl<TranslationLocale[]> {
 
     @Input() public labelKey: string = 'SHARED.LOCALES_LABEL';
 
-    // @ViewChild('localesList', {static: false}) public valueAccessor: DefaultValueAccessor;
+    @ViewChild('localesList', {static: false}) public localesList: ElementRef<HTMLInputElement>;
     @ViewChild('localeInput', {static: false}) public localeInput: ElementRef<HTMLInputElement>;
-    @ViewChild('auto', {static: false}) public matAutocomplete;
+    @ViewChild('auto', {static: false}) public matAutocomplete: MatAutocomplete;
+    @HostBinding('attr.aria-describedby') public describedBy = '';
+    @HostBinding() public id = `app-translation-locale-selector-${TranslationLocaleSelectorComponent.nextId++}`;
 
+    public stateChanges = new Subject<void>();
+    public focused = false;
+    public controlType = 'translation-locale-selector';
+    public errorState = false;
+    public ngControl: NgControl;
+
+    public readonly parts: FormGroup;
     public filteredLocales$: Observable<TranslationLocale[]>;
     public readonly localeInputCtrl = new FormControl();
     public readonly separatorKeysCodes: number[] = [ENTER, COMMA];
@@ -49,7 +71,15 @@ export class TranslationLocaleSelectorComponent implements OnInit/*, ControlValu
     private _required = false;
     private readonly _destroyed$ = new Subject();
 
-    constructor(private _localeService: TranslationLocaleService) {
+    constructor(private injector: Injector,
+                private focusMonitor: FocusMonitor,
+                private formBuilder: FormBuilder,
+                private elRef: ElementRef<HTMLElement>,
+                private _localeService: TranslationLocaleService) {
+        this.parts = formBuilder.group({
+            input: [null],
+            list: [[]]
+        });
     }
 
     ngOnInit() {
@@ -70,30 +100,108 @@ export class TranslationLocaleSelectorComponent implements OnInit/*, ControlValu
                 takeUntil(this._destroyed$),
                 map(([locale, remainingAvailableLocales]) => locale ? this.filterRemaining(locale, remainingAvailableLocales) : remainingAvailableLocales)
             );
+    }
 
-        this.valueChange.emit(this.value.slice());
+    ngAfterViewInit(): void {
+        this.focusMonitor
+            .monitor(this.elRef.nativeElement, true)
+            .subscribe(origin => {
+                this.focused = !!origin;
+                this.stateChanges.next();
+            });
     }
 
     ngOnDestroy(): void {
         this._destroyed$.next();
         this._destroyed$.complete();
+
+        this.focusMonitor.stopMonitoring(this.elRef.nativeElement);
+        this.stateChanges.complete();
     }
 
-    // writeValue(obj: any) {
-    //     this.valueAccessor.writeValue(obj);
-    // }
-    //
-    // registerOnChange(fn: any) {
-    //     this.valueAccessor.registerOnChange(fn);
-    // }
-    //
-    // registerOnTouched(fn: any) {
-    //     this.valueAccessor.registerOnTouched(fn);
-    // }
-    //
-    // setDisabledState(isDisabled: boolean) {
-    //     this.valueAccessor.setDisabledState(isDisabled);
-    // }
+    ngDoCheck(): void {
+        if (this.ngControl) {
+            this.errorState = this.ngControl.invalid && this.ngControl.touched;
+            this.stateChanges.next();
+        }
+    }
+
+    @Input()
+    get placeholder(): string {
+        return this._placeholder;
+    }
+
+    set placeholder(value: string) {
+        this._placeholder = value;
+        this.stateChanges.next();
+    }
+
+    @Input()
+    get disabled(): boolean {
+        return this._disabled;
+    }
+
+    set disabled(value: boolean) {
+        this._disabled = coerceBooleanProperty(value);
+        this._disabled ? this.parts.disable() : this.parts.enable();
+        this.stateChanges.next();
+    }
+
+    @Input()
+    get required() {
+        return this._required;
+    }
+
+    set required(req) {
+        this._required = coerceBooleanProperty(req);
+        this.stateChanges.next();
+    }
+
+    get value(): TranslationLocale[] {
+        return this.parts.controls['list'].value;
+    }
+
+    set value(value: TranslationLocale[]) {
+        this.writeValue(value);
+
+        this.onChange(value);
+        this.stateChanges.next();
+    }
+
+    get empty() {
+        return _.isEmpty(this.parts.controls['list']);
+    }
+
+    @HostBinding('class.floating')
+    get shouldLabelFloat() {
+        return this.focused || !this.empty;
+    }
+
+    setDescribedByIds(ids: string[]) {
+        this.describedBy = ids.join(' ');
+    }
+
+    onContainerClick(event: MouseEvent) {
+        if ((event.target as Element).tagName.toLowerCase() != 'input') {
+            this.elRef.nativeElement.querySelector('input').focus();
+        }
+    }
+
+    writeValue(values: TranslationLocale[] | null): void {
+        this.parts.controls['list'].setValue(values);
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
 
     selected(event: MatAutocompleteSelectedEvent): void {
         this.add(<TranslationLocale><unknown>event.option.value);
@@ -132,6 +240,11 @@ export class TranslationLocaleSelectorComponent implements OnInit/*, ControlValu
         }
     }
 
+    private setValue(value: TranslationLocale[]) {
+        this.value = value;
+        this.stateChanges.next();
+    }
+
     private getIndex(locale: TranslationLocale, locales: TranslationLocale[]) {
         return _.findIndex(locales, first => locale.equals(first));
     }
@@ -141,9 +254,9 @@ export class TranslationLocaleSelectorComponent implements OnInit/*, ControlValu
     }
 
     private filterRemaining(value: string | TranslationLocale, remainingAvailableLocales: TranslationLocale[]): TranslationLocale[] {
-        if(!value){
+        if (!value) {
             return remainingAvailableLocales;
-        } else if(value instanceof TranslationLocale){
+        } else if (value instanceof TranslationLocale) {
             return remainingAvailableLocales.filter(locale => !locale.equals(value));
         } else {
             const filterValue = value.toLowerCase();
