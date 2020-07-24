02db3810ae669bbbca19f5f5e0eede446fd3aacc
Sebastien Gerard <sgerard@emasphere.com>
2020-07-24 07:22:45 +0200

    account

--- front/src/main/web/src/app/account/component/account/account.component.ts
+++ front/src/main/web/src/app/account/component/account/account.component.ts
@@ -1,90 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {UserPreferencesService} from "../../service/user-preferences.service";
-import {ToolLocaleService} from "../../../core/translation/service/tool-locale.service";
-import {ToolLocale} from "../../../core/translation/model/tool-locale.model";
-import {Subject} from "rxjs";
-import {takeUntil} from "rxjs/operators";
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {UserPreferences} from "../../model/user-preferences";
-import {TranslationLocaleService} from "../../../translations/service/translation-locale.service";
-import {TranslationLocale} from "../../../translations/model/translation-locale.model";
-import {NotificationService} from "../../../core/notification/service/notification.service";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-account',
     templateUrl: './account.component.html',
     styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit, OnDestroy {
+export class AccountComponent {
 
-    public form: FormGroup;
-
-    public preferredToolLocale: ToolLocale;
-    public preferredLocales: TranslationLocale[] = [];
-    public loading: boolean = false;
-
-    private readonly _destroyed$ = new Subject();
-
-    constructor(private userPreferencesService: UserPreferencesService,
-                private formBuilder: FormBuilder,
-                private notificationService: NotificationService,
-                public toolLocaleService: ToolLocaleService,
-                public translationLocaleService: TranslationLocaleService) {
-        this.form = formBuilder.group({
-            toolLocale: [],
-            preferredLocales: []
-        });
-    }
-
-    ngOnInit() {
-        this.toolLocaleService
-            .getCurrentLocale()
-            .pipe(takeUntil(this._destroyed$))
-            .subscribe(toolLocale => this.form.controls['toolLocale'].setValue(toolLocale));
-
-        this.toolLocaleService
-            .getPreferredToolLocale()
-            .pipe(takeUntil(this._destroyed$))
-            .subscribe(toolLocale => this.preferredToolLocale = toolLocale);
-
-        this.translationLocaleService
-            .getDefaultLocales()
-            .pipe(takeUntil(this._destroyed$))
-            .subscribe(defaultLocales => this.form.controls['preferredLocales'].setValue(defaultLocales));
-
-        this.translationLocaleService
-            .getPreferredLocales()
-            .pipe(takeUntil(this._destroyed$))
-            .subscribe(preferredLocales => this.preferredLocales = preferredLocales);
-    }
-
-    ngOnDestroy(): void {
-        this._destroyed$.next();
-        this._destroyed$.complete();
-    }
-
-    get toolLocale(): ToolLocale {
-        return this.form.controls['toolLocale'].value;
-    }
-
-    onSave() {
-        this.loading = true;
-
-        this.userPreferencesService
-            .updateUserPreferences(
-                new UserPreferences(
-                    this.toolLocale,
-                    this.form.controls['preferredLocales'].value.map(locale => locale.id)
-                )
-            )
-            .toPromise()
-            .catch(error => {
-                console.error('Error while saving user\'s preferences.', error);
-                this.notificationService.displayErrorMessage('Error while saving user\'s preferences.');
-            })
-            .finally(() => {
-                this.form.markAsPristine();
-                this.loading = false;
-            });
+    constructor() {
     }
 }
--- front/src/main/web/src/app/account/component/account/edit-preferences/edit-preferences.component.ts
+++ front/src/main/web/src/app/account/component/account/edit-preferences/edit-preferences.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ToolLocale} from "../../../../core/translation/model/tool-locale.model";
+import {TranslationLocale} from "../../../../translations/model/translation-locale.model";
+import {Subject} from "rxjs";
+import {UserPreferencesService} from "../../../service/user-preferences.service";
+import {NotificationService} from "../../../../core/notification/service/notification.service";
+import {ToolLocaleService} from "../../../../core/translation/service/tool-locale.service";
+import {TranslationLocaleService} from "../../../../translations/service/translation-locale.service";
+import {takeUntil} from "rxjs/operators";
+import {UserPreferences} from "../../../model/user-preferences";
 
 @Component({
   selector: 'app-edit-preferences',
   templateUrl: './edit-preferences.component.html',
   styleUrls: ['./edit-preferences.component.css']
 })
-export class EditPreferencesComponent implements OnInit {
+export class EditPreferencesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public form: FormGroup;
 
-  ngOnInit() {
+  public preferredToolLocale: ToolLocale;
+  public preferredLocales: TranslationLocale[] = [];
+  public loading: boolean = false;
+
+  private readonly _destroyed$ = new Subject();
+
+  constructor(private userPreferencesService: UserPreferencesService,
+              private formBuilder: FormBuilder,
+              private notificationService: NotificationService,
+              public toolLocaleService: ToolLocaleService,
+              public translationLocaleService: TranslationLocaleService) {
+    this.form = formBuilder.group({
+      toolLocale: [],
+      preferredLocales: []
+    });
   }
 
+  ngOnInit() {
+    this.toolLocaleService
+        .getCurrentLocale()
+        .pipe(takeUntil(this._destroyed$))
+        .subscribe(toolLocale => this.form.controls['toolLocale'].setValue(toolLocale));
+
+    this.toolLocaleService
+        .getPreferredToolLocale()
+        .pipe(takeUntil(this._destroyed$))
+        .subscribe(toolLocale => this.preferredToolLocale = toolLocale);
+
+    this.translationLocaleService
+        .getDefaultLocales()
+        .pipe(takeUntil(this._destroyed$))
+        .subscribe(defaultLocales => this.form.controls['preferredLocales'].setValue(defaultLocales));
+
+    this.translationLocaleService
+        .getPreferredLocales()
+        .pipe(takeUntil(this._destroyed$))
+        .subscribe(preferredLocales => this.preferredLocales = preferredLocales);
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+
+  get toolLocale(): ToolLocale {
+    return this.form.controls['toolLocale'].value;
+  }
+
+  onSave() {
+    this.loading = true;
+
+    this.userPreferencesService
+        .updateUserPreferences(
+            new UserPreferences(
+                this.toolLocale,
+                this.form.controls['preferredLocales'].value.map(locale => locale.id)
+            )
+        )
+        .toPromise()
+        .catch(error => {
+          console.error('Error while saving user\'s preferences.', error);
+          this.notificationService.displayErrorMessage('Error while saving user\'s preferences.');
+        })
+        .finally(() => {
+          this.form.markAsPristine();
+          this.loading = false;
+        });
+  }
 }
