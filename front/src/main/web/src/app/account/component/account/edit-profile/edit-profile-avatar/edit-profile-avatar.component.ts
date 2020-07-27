2bb32ef923a68bd91b4298cd7ee765ea17154768
Sebastien Gerard <sgerard@emasphere.com>
2020-07-27 20:06:17 +0200

    avatar

--- front/src/main/web/src/app/account/component/account/edit-profile/edit-profile-avatar/edit-profile-avatar.component.ts
+++ front/src/main/web/src/app/account/component/account/edit-profile/edit-profile-avatar/edit-profile-avatar.component.ts
@@ -2,33 +2,50 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {User} from "../../../../../core/auth/model/user.model";
 import {Subject} from "rxjs";
 import {AuthenticationService} from "../../../../../core/auth/service/authentication.service";
+import {DroppedFile} from "../../../../../core/shared/directive/drag-drop.directive";
 
 @Component({
-  selector: 'app-edit-profile-avatar',
-  templateUrl: './edit-profile-avatar.component.html',
-  styleUrls: ['./edit-profile-avatar.component.css']
+    selector: 'app-edit-profile-avatar',
+    templateUrl: './edit-profile-avatar.component.html',
+    styleUrls: ['./edit-profile-avatar.component.css']
 })
-export class EditProfileAvatarComponent implements OnInit, OnDestroy  {
+export class EditProfileAvatarComponent implements OnInit, OnDestroy {
 
-  public currentUser: User;
-  private readonly _destroyed$ = new Subject();
+    public currentUser: User;
 
-  constructor(private authenticationService: AuthenticationService) {
-  }
+    public _currentUserAvatar: string;
+    public _uploadUserAvatar: any;
 
-  ngOnInit() {
-    this.authenticationService.currentUser()
-        .subscribe(currentUser => this.currentUser = currentUser);
-  }
+    private readonly _destroyed$ = new Subject();
 
-  ngOnDestroy(): void {
-    this._destroyed$.next();
-    this._destroyed$.complete();
-  }
+    constructor(private authenticationService: AuthenticationService) {
+    }
 
-  public getAvatarUrl(): string {
-    return (this.currentUser != null)
-        ? `url('/api/user/${this.currentUser.id}/avatar')`
-        : null;
-  }
+    public ngOnInit() {
+        this.authenticationService.currentUser()
+            .subscribe(currentUser => {
+                this.currentUser = currentUser;
+
+                this._currentUserAvatar = (this.currentUser != null)
+                    ? `url('/api/user/${this.currentUser.id}/avatar')`
+                    : null;
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this._destroyed$.next();
+        this._destroyed$.complete();
+    }
+
+    public getAvatarUrl(): any {
+        return this._uploadUserAvatar ? this._uploadUserAvatar : this._currentUserAvatar;
+    }
+
+    public onFileDropped(file: DroppedFile) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.file);
+        reader.onload = (_event) => {
+            this._uploadUserAvatar = `url(${reader.result})`;
+        };
+    }
 }
--- front/src/main/web/src/app/core/shared/directive/drag-drop.directive.ts
+++ front/src/main/web/src/app/core/shared/directive/drag-drop.directive.ts
@@ -1,42 +1,66 @@
-import { Directive, Output, EventEmitter, HostBinding, HostListener } from '@angular/core';
+import {Directive, EventEmitter, HostBinding, HostListener, Output} from '@angular/core';
+
+export class DroppedFile {
+
+    constructor(public file: File, public contentType: string) {
+    }
+}
+
+export let FILE_TYPES = new Map([
+    ['jpg', 'image/jpeg'],
+    ['jpeg', 'image/jpeg'],
+    ['png', 'image/png'],
+]);
 
 @Directive({
-  selector: '[appDragDrop]'
+    selector: '[appDragDrop]'
 })
 export class DragDropDirective {
 
-  @Output() onFileDropped = new EventEmitter<any>();
+    @Output() onFileDropped = new EventEmitter<DroppedFile>();
 
-  @HostBinding('class.dragAndDropZone') private dragAndDropZoneClass = true;
-  @HostBinding('class.dragAndDropZoneOver') private dragAndDropZoneOverClass = false;
+    @HostBinding('class.dragAndDropZone') private dragAndDropZoneClass = true;
+    @HostBinding('class.dragAndDropZoneOver') private dragAndDropZoneOverClass = false;
 
-  //Dragover listener
-  @HostListener('dragover', ['$event']) onDragOver(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
+    @HostListener('dragover', ['$event']) onDragOver(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
 
-    this.dragAndDropZoneOverClass = true;
-  }
-
-  //Dragleave listener
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    this.dragAndDropZoneOverClass = false;
-  }
-
-  //Drop listener
-  @HostListener('drop', ['$event']) public ondrop(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-
-    this.dragAndDropZoneOverClass = false;
-
-    let files = evt.dataTransfer.files;
-    if (files.length > 0) {
-      this.onFileDropped.emit(files)
+        this.dragAndDropZoneOverClass = true;
     }
-  }
 
+    @HostListener('dragleave', ['$event'])
+    public onDragLeave(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        this.dragAndDropZoneOverClass = false;
+    }
+
+    @HostListener('drop', ['$event'])
+    public ondrop(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        this.dragAndDropZoneOverClass = false;
+
+        const dataTransfer: DataTransfer = evt.dataTransfer;
+
+        if (evt.dataTransfer.files.length == 1) {
+            this.onFileDropped.emit(this.createDroppedFile(dataTransfer.files[0]))
+        }
+    }
+
+    private createDroppedFile(file: File): DroppedFile {
+        let contentType = file.type;
+
+        for (const extension of FILE_TYPES.keys()) {
+            if (file.name.toLowerCase().endsWith(`.${extension}`)) {
+                contentType = FILE_TYPES.get(extension)
+                break;
+            }
+        }
+
+        return new DroppedFile(file, contentType);
+    }
 }
