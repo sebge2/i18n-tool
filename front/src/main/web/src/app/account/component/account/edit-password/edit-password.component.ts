8ac98e68ee0d8c2d30152edb8eb69e6af604ed9b
Sebastien Gerard <sgerard@emasphere.com>
2020-07-30 07:29:52 +0200

    user

--- front/src/main/web/src/app/account/component/account/edit-password/edit-password.component.ts
+++ front/src/main/web/src/app/account/component/account/edit-password/edit-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-password',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPasswordComponent implements OnInit {
 
-  constructor() { }
+  public readonly form: FormGroup;
+  public loading = false;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group(
+        {
+          currentPassword: this.formBuilder.control('', [Validators.required]),
+          newPassword: this.formBuilder.control('', [Validators.minLength(6), Validators.required]),
+          confirmedPassword: this.formBuilder.control('', []) // check confirmed
+        }
+    );
   }
 
+  public ngOnInit() {
+  }
+
+  public onSave() {
+
+  }
+
+  public resetForm() {
+
+  }
 }
