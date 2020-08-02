95e531905583e7b4c713fe0736fb1bebd0b9a93a
Sebastien Gerard <sgerard@emasphere.com>
2020-08-02 08:47:26 +0200

    repository initializer

--- front/src/main/web/src/app/admin/admin.module.ts
+++ front/src/main/web/src/app/admin/admin.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {WorkspaceTableComponent} from './component/workspace-table/workspace-table.component';
-import {RepositoryInitializerComponent} from './component/repository-initializer/repository-initializer.component';
 import {CoreSharedModule} from "../core/shared/core-shared-module";
 import {ConfirmWorkspaceDeletionComponent} from './component/workspace-table/confirm-deletion/confirm-workspace-deletion.component';
 import {CoreAuthModule} from "../core/auth/core-auth.module";
@@ -10,15 +9,15 @@ import {UsersComponent} from './component/users/users.component';
 import {RepositoriesComponent} from './component/repositories/repositories.component';
 import {LocalesComponent} from './component/locales/locales.component';
 import {LocaleViewCardComponent} from './component/locales/locale-view-card/locale-view-card.component';
-import { RepositoryViewCardComponent } from './component/repositories/repository-view-card/repository-view-card.component';
-import { UserViewCardComponent } from './component/users/user-view-card/user-view-card.component';
-import { RepositoryDetailsComponent } from './component/repositories/repository-details/repository-details.component';
-import { RepositoryListComponent } from './component/repositories/repository-list/repository-list.component';
-import { RepositoryAddWizardComponent } from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard.component';
-import { RepositoryAddWizardStepTypeComponent } from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-type/repository-add-wizard-step-type.component';
-import { RepositoryAddWizardStepInfoComponent } from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-info/repository-add-wizard-step-info.component';
-import { RepositoryAddWizardStepCreationComponent } from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-creation/repository-add-wizard-step-creation.component';
-import { RepositoryAddWizardStepInitializationComponent } from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-initialization/repository-add-wizard-step-initialization.component';
+import {RepositoryViewCardComponent} from './component/repositories/repository-view-card/repository-view-card.component';
+import {UserViewCardComponent} from './component/users/user-view-card/user-view-card.component';
+import {RepositoryDetailsComponent} from './component/repositories/repository-details/repository-details.component';
+import {RepositoryListComponent} from './component/repositories/repository-list/repository-list.component';
+import {RepositoryAddWizardComponent} from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard.component';
+import {RepositoryAddWizardStepTypeComponent} from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-type/repository-add-wizard-step-type.component';
+import {RepositoryAddWizardStepInfoComponent} from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-info/repository-add-wizard-step-info.component';
+import {RepositoryAddWizardStepCreationComponent} from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-creation/repository-add-wizard-step-creation.component';
+import {RepositoryAddWizardStepInitializationComponent} from './component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-initialization/repository-add-wizard-step-initialization.component';
 
 const appRoutes: Routes = [
     {path: 'users', component: UsersComponent},
@@ -29,7 +28,6 @@ const appRoutes: Routes = [
 @NgModule({
     declarations: [
         WorkspaceTableComponent,
-        RepositoryInitializerComponent,
         ConfirmWorkspaceDeletionComponent,
         LocalesComponent,
         LocaleViewCardComponent,
