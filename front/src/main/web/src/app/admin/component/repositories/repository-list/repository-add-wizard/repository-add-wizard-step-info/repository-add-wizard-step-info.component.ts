b05c8068fabee6e4ee7e1eeb9328a9abfea54efe
Sebastien Gerard <sgerard@emasphere.com>
2020-08-03 17:16:49 +0200

    creation

--- front/src/main/web/src/app/admin/component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-info/repository-add-wizard-step-info.component.ts
+++ front/src/main/web/src/app/admin/component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-info/repository-add-wizard-step-info.component.ts
@@ -1,13 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {RepositoryType} from "../../../../../../translations/model/repository-type.model";
+import {GitHubRepositoryCreationRequestDto, GitRepositoryCreationRequestDto} from "../../../../../../api";
 
 @Component({
     selector: 'app-repository-add-wizard-step-info',
     templateUrl: './repository-add-wizard-step-info.component.html',
     styleUrls: ['./repository-add-wizard-step-info.component.css']
 })
-export class RepositoryAddWizardStepInfoComponent implements OnInit {
+export class RepositoryAddWizardStepInfoComponent {
 
     @Input() public form: FormGroup;
 
@@ -16,13 +17,6 @@ export class RepositoryAddWizardStepInfoComponent implements OnInit {
     private _repositoryType: RepositoryType;
 
     constructor(private formBuilder: FormBuilder) {
-    }
-
-    ngOnInit() {
-    }
-
-    onSubmit() {
-
     }
 
     @Input()
@@ -40,6 +34,25 @@ export class RepositoryAddWizardStepInfoComponent implements OnInit {
             this.form.setControl('username', this.formBuilder.control('', [Validators.required]));
             this.form.setControl('repository', this.formBuilder.control('', [Validators.required]));
             this.form.setControl('accessKey', this.formBuilder.control('', []));
+        }
+    }
+
+    public get creationRequest(): (GitHubRepositoryCreationRequestDto | GitRepositoryCreationRequestDto) {
+        if (this.repositoryType == RepositoryType.GIT) {
+            return <GitRepositoryCreationRequestDto>{
+                type: "GIT",
+                location: this.form.controls['location'].value,
+                name: this.form.controls['name'].value
+            };
+        } else if (this.repositoryType == RepositoryType.GITHUB) {
+            return <GitHubRepositoryCreationRequestDto>{
+                type: "GITHUB",
+                username: this.form.controls['username'].value,
+                repository: this.form.controls['repository'].value,
+                accessKey: this.form.controls['accessKey'].value,
+            };
+        } else {
+            throw Error(`Unsupported repository type ${this.repositoryType}.`);
         }
     }
 }
