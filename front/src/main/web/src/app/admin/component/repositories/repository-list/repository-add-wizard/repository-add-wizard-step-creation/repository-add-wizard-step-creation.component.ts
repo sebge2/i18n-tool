71694f62770763b65d0636095edb94466fab759e
Sebastien Gerard <sgerard@emasphere.com>
2020-08-04 18:54:34 +0200

    wizard

---
+++ front/src/main/web/src/app/admin/component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-creation/repository-add-wizard-step-creation.component.ts
@@ -0,0 +1,65 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {RepositoryCreationRequestDto} from "../../../../../../api";
+import {RepositoryService} from "../../../../../../translations/service/repository.service";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
+import {FormGroup} from "@angular/forms";
+import {Repository} from "../../../../../../translations/model/repository.model";
+
+@Component({
+    selector: 'app-repository-add-wizard-step-creation',
+    templateUrl: './repository-add-wizard-step-creation.component.html',
+    styleUrls: ['./repository-add-wizard-step-creation.component.css']
+})
+export class RepositoryAddWizardStepCreationComponent implements OnInit, OnDestroy {
+
+    @Input() public form: FormGroup;
+
+    public creationInProgress = false;
+
+    public error: any;
+
+    private _creationRequest: RepositoryCreationRequestDto;
+    private _destroyed$ = new Subject<void>();
+
+    constructor(private repositoryService: RepositoryService) {
+    }
+
+    public ngOnInit() {
+    }
+
+    public ngOnDestroy(): void {
+        this._destroyed$.next();
+        this._destroyed$.complete();
+    }
+
+    @Input()
+    public get creationRequest(): RepositoryCreationRequestDto {
+        return this._creationRequest;
+    }
+
+    public set creationRequest(value: RepositoryCreationRequestDto) {
+        this._creationRequest = value;
+
+        if (this._creationRequest) {
+            this.creationInProgress = true;
+            this.repository = null;
+
+            this.repositoryService
+                .createRepository(this.creationRequest)
+                .pipe(takeUntil(this._destroyed$))
+                .toPromise()
+                .then(repository => this.repository = repository)
+                .catch(error => this.error = error)
+                .finally(() => this.creationInProgress = false);
+        }
+    }
+
+    public get repository(): Repository {
+        return this.form.controls['repository'].value;
+    }
+
+    public set repository(repository: Repository) {
+        this.form.controls['repository'].setValue(repository);
+    }
+}
