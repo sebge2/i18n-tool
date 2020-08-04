86f784b526f6707ca9b15efb68b0eaf642be1366
Sebastien Gerard <sgerard@emasphere.com>
2020-08-04 20:06:18 +0200

    login

--- front/src/main/web/src/app/admin/component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-creation/repository-add-wizard-step-creation.component.ts
+++ front/src/main/web/src/app/admin/component/repositories/repository-list/repository-add-wizard/repository-add-wizard-step-creation/repository-add-wizard-step-creation.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnDestroy} from '@angular/core';
-import {RepositoryCreationRequestDto} from "../../../../../../api";
+import {ErrorMessagesDto, RepositoryCreationRequestDto} from "../../../../../../api";
 import {RepositoryService} from "../../../../../../translations/service/repository.service";
 import {Subject} from "rxjs";
 import {takeUntil} from "rxjs/operators";
 import {FormGroup} from "@angular/forms";
 import {Repository} from "../../../../../../translations/model/repository.model";
+import {instanceOfErrorMessages, instanceOfHttpError} from "../../../../../../core/shared/utils/error-utils";
 
 @Component({
     selector: 'app-repository-add-wizard-step-creation',
@@ -16,8 +17,8 @@ export class RepositoryAddWizardStepCreationComponent implements OnDestroy {
     @Input() public form: FormGroup;
 
     public creationInProgress = false;
-
-    public error: any;
+    public unknownError: any;
+    public errorMessages: ErrorMessagesDto;
 
     private _creationRequest: RepositoryCreationRequestDto;
     private _destroyed$ = new Subject<void>();
@@ -37,17 +38,17 @@ export class RepositoryAddWizardStepCreationComponent implements OnDestroy {
 
     public set creationRequest(value: RepositoryCreationRequestDto) {
         this._creationRequest = value;
+        this.repository = null;
 
         if (this._creationRequest) {
             this.creationInProgress = true;
-            this.repository = null;
 
             this.repositoryService
                 .createRepository(this.creationRequest)
                 .pipe(takeUntil(this._destroyed$))
                 .toPromise()
                 .then(repository => this.repository = repository)
-                .catch(error => this.error = error)
+                .catch(error => this.handleError(error))
                 .finally(() => this.creationInProgress = false);
         }
     }
@@ -60,7 +61,13 @@ export class RepositoryAddWizardStepCreationComponent implements OnDestroy {
         this.form.controls['repository'].setValue(repository);
     }
 
-    public get successful(): boolean {
-        return !!this.repository;
+    private handleError(cause: any) {
+        if (instanceOfHttpError(cause)) {
+            return this.handleError(cause.error);
+        } else if (instanceOfErrorMessages(cause)) {
+            this.errorMessages = <ErrorMessagesDto>cause;
+        } else {
+            this.unknownError = cause;
+        }
     }
 }
