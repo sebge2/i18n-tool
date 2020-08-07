b4ad521f615a856569e839c054ee484531dcb585
Sebastien Gerard <sgerard@emasphere.com>
2020-08-07 19:41:53 +0200

    button

--- front/src/main/web/src/app/core/shared/component/form-sync-button/form-sync-button.component.ts
+++ front/src/main/web/src/app/core/shared/component/form-sync-button/form-sync-button.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
     selector: 'app-form-sync-button',
@@ -7,6 +7,8 @@ import {Component, EventEmitter, Output} from '@angular/core';
 })
 export class FormSyncButtonComponent {
 
+    @Input() public syncInProgress: boolean;
+    @Input() public disabled: boolean;
     @Output() public sync = new EventEmitter<void>();
 
     constructor() {
