93a8fff5af6532d63851d430f9c61f0a806ae7ed
Sebastien Gerard <sgerard@emasphere.com>
2020-08-13 15:38:10 +0200

    badge

--- front/src/main/web/src/app/admin/component/repositories/repository-details/repository-details-workspaces/repository-details-workspace-node/repository-details-workspace-node.component.ts
+++ front/src/main/web/src/app/admin/component/repositories/repository-details/repository-details-workspaces/repository-details-workspace-node/repository-details-workspace-node.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {WorkspaceTreeNode} from "../repository-details-workspaces.component";
+import {WorkspaceStatus} from "../../../../../../translations/model/workspace/workspace-status.model";
 
 @Component({
     selector: 'app-repository-details-workspace-node',
@@ -9,12 +10,36 @@ import {WorkspaceTreeNode} from "../repository-details-workspaces.component";
 export class RepositoryDetailsWorkspaceNodeComponent {
 
     @Input() public node: WorkspaceTreeNode;
-
     constructor() {
     }
 
     public get name(): string {
         return this.node.workspace.branch;
+    }
+
+    public get badgeClass(): string {
+        switch(this.node.workspace.status){
+            case WorkspaceStatus.IN_REVIEW:
+            case WorkspaceStatus.INITIALIZED:
+                return 'app-badge-success';
+            case WorkspaceStatus.NOT_INITIALIZED:
+                return 'app-badge-warning';
+            default:
+                return '';
+        }
+    }
+
+    public get badgeText(): string {
+        switch(this.node.workspace.status){
+            case WorkspaceStatus.IN_REVIEW:
+                return 'ADMIN.REPOSITORIES.DETAILS_CARD.WORKSPACES.IN_REVIEW';
+            case WorkspaceStatus.INITIALIZED:
+                return 'ADMIN.REPOSITORIES.DETAILS_CARD.WORKSPACES.READY';
+            case WorkspaceStatus.NOT_INITIALIZED:
+                return 'ADMIN.REPOSITORIES.DETAILS_CARD.WORKSPACES.NOT_READY';
+            default:
+                return '';
+        }
     }
 
 }
