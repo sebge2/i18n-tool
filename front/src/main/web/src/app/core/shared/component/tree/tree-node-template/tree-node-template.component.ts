5e3a18fbf30a785d1d80576e15a8866212310b43
Sebastien Gerard <sgerard@emasphere.com>
2020-08-15 09:26:19 +0200

    tree

--- front/src/main/web/src/app/core/shared/component/tree/tree-node-template/tree-node-template.component.ts
+++ front/src/main/web/src/app/core/shared/component/tree/tree-node-template/tree-node-template.component.ts
@@ -6,6 +6,7 @@ import {
     ComponentRef,
     Input,
     Type,
+    ViewChild,
     ViewContainerRef
 } from '@angular/core';
 import {TreeNode} from "../tree.component";
@@ -19,14 +20,14 @@ export class TreeNodeTemplateComponent implements AfterViewInit {
 
     @Input() public node: TreeNode;
     @Input() public nodeComponent: Type<any>;
+    @ViewChild('target', {read: ViewContainerRef}) public target: ViewContainerRef;
 
-    constructor(private resolver: ComponentFactoryResolver,
-                private _viewContainerRef: ViewContainerRef) {
+    constructor(private resolver: ComponentFactoryResolver) {
     }
 
     ngAfterViewInit() {
         let factory: ComponentFactory<any> = this.resolver.resolveComponentFactory(this.nodeComponent);
-        let component: ComponentRef<any> = this._viewContainerRef.createComponent(factory);
+        let component: ComponentRef<any> = this.target.createComponent(factory);
         component.instance.node = this.node;
     }
 }
