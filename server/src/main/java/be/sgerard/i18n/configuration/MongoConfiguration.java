e9bb17e2e293e9418c7b5a4e9197879dd44919a5
Sebastien Gerard <sgerard@emasphere.com>
2020-08-24 21:11:59 +0200

    create db

--- server/src/main/java/be/sgerard/i18n/configuration/MongoConfiguration.java
+++ server/src/main/java/be/sgerard/i18n/configuration/MongoConfiguration.java
@@ -2,22 +2,11 @@ package be.sgerard.i18n.configuration;
 
 import be.sgerard.i18n.model.support.PatternToStringConverter;
 import be.sgerard.i18n.model.support.StringToPatternConverter;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
-import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -34,13 +23,7 @@ import static java.util.Arrays.asList;
 @EnableTransactionManagement
 public class MongoConfiguration {
 
-    private final MongoConverter mongoConverter;
-    private final MongoTemplate mongoTemplate;
-
-    @Lazy
-    public MongoConfiguration(MongoConverter mongoConverter, MongoTemplate mongoTemplate) {
-        this.mongoConverter = mongoConverter;
-        this.mongoTemplate = mongoTemplate;
+    public MongoConfiguration() {
     }
 
     @Bean
@@ -54,23 +37,5 @@ public class MongoConfiguration {
                 new PatternToStringConverter(),
                 new StringToPatternConverter()
         ));
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void initIndicesAfterStartup() {
-        final MappingContext<?, ?> mappingContext = mongoConverter.getMappingContext();
-
-        if (mappingContext instanceof MongoMappingContext) {
-            MongoMappingContext mongoMappingContext = (MongoMappingContext) mappingContext;
-            for (BasicMongoPersistentEntity<?> persistentEntity : mongoMappingContext.getPersistentEntities()) {
-                var clazz = persistentEntity.getType();
-                if (clazz.isAnnotationPresent(Document.class)) {
-                    var resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-
-                    final IndexOperations indexOps = mongoTemplate.indexOps(clazz);
-                    resolver.resolveIndexFor(clazz).forEach(indexOps::ensureIndex);
-                }
-            }
-        }
     }
 }
---
+++ server/src/main/java/be/sgerard/i18n/service/support/MongoHelper.java
@@ -0,0 +1,77 @@
+package be.sgerard.i18n.service.support;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mapping.model.BasicPersistentEntity;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Helper for MongoDB.
+ *
+ * @author Sebastien Gerard
+ */
+@Component
+public class MongoHelper {
+
+    private final MongoConverter mongoConverter;
+    private final MongoTemplate mongoTemplate;
+
+    public MongoHelper(MongoConverter mongoConverter, MongoTemplate mongoTemplate) {
+        this.mongoConverter = mongoConverter;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    /**
+     * Returns all the document tpes.
+     */
+    public Collection<? extends Class<?>> getDocumentTypes() {
+        return Optional
+                .of(mongoConverter.getMappingContext())
+                .filter(MongoMappingContext.class::isInstance)
+                .map(context -> (MongoMappingContext) context)
+                .map(mongoMappingContext ->
+                        mongoMappingContext.getPersistentEntities()
+                                .stream()
+                                .filter(persistentEntity -> persistentEntity.getType().isAnnotationPresent(Document.class))
+                                .map(BasicPersistentEntity::getType)
+                                .collect(toList())
+                )
+                .orElseGet(Collections::emptyList);
+    }
+
+    /**
+     * Setup indexes and documents.
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() {
+        final MappingContext<?, ?> mappingContext = mongoConverter.getMappingContext();
+
+        if (mappingContext instanceof MongoMappingContext) {
+            final MongoMappingContext mongoMappingContext = (MongoMappingContext) mappingContext;
+
+            var resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
+            getDocumentTypes()
+                    .forEach(documentType -> {
+                        if (!mongoTemplate.collectionExists(documentType)) {
+                            mongoTemplate.createCollection(documentType);
+                        }
+
+                        final IndexOperations indexOps = mongoTemplate.indexOps(documentType);
+                        resolver.resolveIndexFor(documentType).forEach(indexOps::ensureIndex);
+                    });
+        }
+    }
+}
