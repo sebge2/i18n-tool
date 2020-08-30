8e312e17baa6e07bbcfa0d7a0cb00b53187e4a67
Sebastien Gerard <sgerard@emasphere.com>
2020-08-30 11:58:39 +0200

    users

--- server/src/main/java/be/sgerard/i18n/model/i18n/dto/TranslationsUpdateEventDto.java
+++ server/src/main/java/be/sgerard/i18n/model/i18n/dto/TranslationsUpdateEventDto.java
@@ -1,28 +1,38 @@
 package be.sgerard.i18n.model.i18n.dto;
 
-import be.sgerard.i18n.model.security.user.dto.UserDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
 
-import java.util.List;
-
+import java.util.Collection;
 
 /**
+ * Event sent when translations have been updated.
+ *
  * @author Sebastien Gerard
  */
+@Getter
+@Builder(builderClassName = "Builder")
+@JsonDeserialize(builder = TranslationsUpdateEventDto.Builder.class)
 public class TranslationsUpdateEventDto {
 
-    private final UserDto user;
-    private final List<BundleKeyTranslationDto> updatedEntries;
+    @Schema(description = "Id of the editor.", required = true)
+    private final String userId;
 
-    public TranslationsUpdateEventDto(UserDto user, List<BundleKeyTranslationDto> updatedEntries) {
-        this.user = user;
-        this.updatedEntries = updatedEntries;
-    }
-// TODO
-    public UserDto getUser() {
-        return user;
+    @Schema(description = "Display name of the editor.", required = true)
+    private final String userDisplayName;
+
+    @Schema(description = "Updated translations.", required = true)
+    @Singular
+    private final Collection<BundleKeyTranslationDto> translations;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static final class Builder {
     }
 
-    public List<BundleKeyTranslationDto> getUpdatedEntries() {
-        return updatedEntries;
-    }
 }
