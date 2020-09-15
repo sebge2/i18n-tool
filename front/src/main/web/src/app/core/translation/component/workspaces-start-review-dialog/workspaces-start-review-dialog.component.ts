6f45e2c416da92778a5fb6e201ef75c754d7bdfb
Sebastien Gerard <sgerard@emasphere.com>
2020-09-15 07:54:20 +0200

    workspaces

--- front/src/main/web/src/app/core/translation/component/workspaces-start-review-dialog/workspaces-start-review-dialog.component.ts
+++ front/src/main/web/src/app/core/translation/component/workspaces-start-review-dialog/workspaces-start-review-dialog.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-workspaces-start-review-dialog',
-  templateUrl: './workspaces-start-review-dialog.component.html',
-  styleUrls: ['./workspaces-start-review-dialog.component.css']
+    selector: 'app-workspaces-start-review-dialog',
+    templateUrl: './workspaces-start-review-dialog.component.html',
+    styleUrls: ['./workspaces-start-review-dialog.component.css']
 })
-export class WorkspacesStartReviewDialogComponent implements OnInit {
+export class WorkspacesStartReviewDialogComponent {
 
-  constructor() { }
+    public readonly form: FormGroup;
+    public publishInProgress: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private dialogRef: MatDialogRef<WorkspacesStartReviewDialogComponent>,
+                private formBuilder: FormBuilder,
+                @Inject(MAT_DIALOG_DATA) public data: any) {
+        this.form = formBuilder.group({
+            comment: ['', Validators.required],
+            workspaces: [[], Validators.required],
+        });
+    }
 
+    public onPublish() {
+        console.log(this.form.controls['workspaces'].value);
+        console.log(this.form.controls['comment'].value);
+    }
 }
