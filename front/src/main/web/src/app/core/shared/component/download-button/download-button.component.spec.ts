357fa89affc92b6ee488ddc64225a74c5bcfe759
Sebastien Gerard <sgerard@emasphere.com>
2020-12-05 08:44:17 +0100

    ignore tests

--- front/src/main/web/src/app/core/shared/component/download-button/download-button.component.spec.ts
+++ front/src/main/web/src/app/core/shared/component/download-button/download-button.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DownloadButtonComponent } from './download-download-button.component';
+import { DownloadButtonComponent } from './download-button.component';
 
 describe('InitDownloadButtonComponent', () => {
   let component: DownloadButtonComponent;
