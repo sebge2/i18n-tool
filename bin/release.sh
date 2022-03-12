33ec3b0c8964cc2ce35c9cd56eddd2a3d92a19de
Sébastien Gérard <me@sgerard.be>
2022-03-12 09:25:54 +0100

    xx

--- bin/release.sh
+++ bin/release.sh
@@ -3,37 +3,22 @@
 set -e
 
 echo "What the release version? (ex: 0.0.6)"
-#read RELEASE_VERSION
-RELEASE_VERSION="0.0.10"
+read RELEASE_VERSION
 echo "Releasing [$RELEASE_VERSION]"
 
 echo "What the next version? (ex: 0.0.7)"
-#read NEXT_VERSION
-NEXT_VERSION="0.0.11"
+read NEXT_VERSION
 echo "Next version is [$NEXT_VERSION]"
-
-mvn -B release:clean release:prepare -DreleaseVersion="$RELEASE_VERSION" -DdevelopmentVersion="$NEXT_VERSION"
-
 
 #git checkout develop
 #git pull --all
 #
-#
-#mvn versions:set -DnewVersion="$RELEASE_VERSION"
-#cd front/src/main/web/
-#npm version "$RELEASE_VERSION" --allow-same-version
-#cd ../../../../
-#git commit -a -m "Set release version $RELEASE_VERSION"
-#git push
-#
-#
 #git checkout master
 #git pull
 #git merge --no-ff develop -m "Release $RELEASE_VERSION"
-#git tag "$RELEASE_VERSION"
-#git push --tags
-#
-#
+
+mvn -B release:clean release:prepare release:perform -DreleaseVersion="$RELEASE_VERSION" -DdevelopmentVersion="$NEXT_VERSION"
+
 #git checkout develop
 #git pull
 #git merge --no-ff master -m "Final merge $RELEASE_VERSION -> develop"
